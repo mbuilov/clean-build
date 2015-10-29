0da3bba43ecfc0461d7fb6c02a4478454bfd08f0
Michael Builov <mbuilov@s-terra.com>
2015-10-29 20:35:19 +0300

    fix SED_EXPR for SOLARIS

--- SOLARIS/make_tools.mk
+++ SOLARIS/make_tools.mk
@@ -12,7 +12,7 @@ DEL   = rm -f $1
 RM    = $(if $(VERBOSE:1=),@)rm -rf $1
 MKDIR = mkdir -p $1
 SED  := sed
-SED_EXPR = '$1'
+SED_EXPR = '$(subst \n,\$(newline),$(subst \t,\$(tab),$1))'
 CAT   = cat $1
 ECHO  = printf '$(subst ','"'"',$(subst $(newline),\n,$(subst \,\\,$(subst %,%%,$1))))\n'
 CD    = cd $1
