d0318c654e468032599aa61e40dbddf5493193ef
Michael Builov <mbuilov@s-terra.com>
2015-11-18 13:49:41 +0300

    add support for .scala compilation

--- make_jheader.mk
+++ make_jheader.mk
@@ -26,51 +26,66 @@ DEBUG_JTARGETS := $(call GET_DEBUG_TARGETS,$(BLD_JTARGETS),FORM_JTRG)
 FORM_JTRG = $(if \
             $(filter JAR,$1),$(BIN_DIR)/$($1)$(JAREXT))
 
-JAVAC_OPTIONS := $(if $(JLINT),-Xlint) $(if $(DEBUG),-g)
-
 # $1 - list of bundles to add to the .jar: dir1 name1 dir2 name2...
 FORM_JAR_BUNDLES = $(if $1,$(call qpath,$(call ospath,$(call FIXPATH,$(firstword $1))),-C ) $(call \
   qpath,$(call ospath,$(word 2,$1))) $(call FORM_JAR_BUNDLES,$(wordlist 3,999999,$1)))
 
+# dir1 name1 dir2 name2... -> dir1/name1 dir2/name2...
+MAKE_BUNDLE_DEPS = $(if $1,$(VPREFIX)$(firstword $1)/$(word 2,$1) $(call MAKE_BUNDLE_DEPS,$(wordlist 3,999999,$1)))
+
 # $1 - entries for classpath list
 FORM_CLASS_PATH = -classpath $(call qpath,$(subst $(space),$(PATHSEP),$(strip $(ospath))))
 
-# $1 - sources
-JAVA_CC1 = $(call SUPRESS,JAVAC,$1) $(JAVAC) $(JAVAC_OPTIONS) $(JCFLAGS) -d $(call ospath,$(OBJDIR)) $(ospath) $(call \
+JAVAC_OPTIONS  := $(if $(JLINT),-Xlint) $(if $(DEBUG),-g)
+SCALAC_OPTIONS := $(if $(DEBUG),-g:vars)
+
+# compile $1 - .java sources
+JAVA_CC1 = $(call SUPRESS,JAVAC,$1)$(JAVAC) $(JAVAC_OPTIONS) $(JAVAC_FLAGS) -d $(call ospath,$(OBJDIR)) $(ospath) $(call \
   FORM_CLASS_PATH,$(OBJDIR) $(CLASSPATH) $(EXTJARS) $(JARS))$(newline)
 JAVA_CC  = $(if $1,$(JAVA_CC1))
 
-# $1 - target
+# compile $1 - .scala, parse $2 - .java sources
+SCALA_CC1 = $(call SUPRESS,SCALAC,$1)$(SCALAC) $(SCALAC_OPTIONS) -d $(call ospath,$(OBJDIR)) $(call ospath,$1 $2) $(call \
+  FORM_CLASS_PATH,$(OBJDIR) $(CLASSPATH) $(EXTJARS) $(JARS))$(newline)
+SCALA_CC  = $(if $1,$(if $(SCALAC),$(SCALA_CC1),$(error \
+  SCALAC not defined, example: $$(JAVA) $$(call FORM_CLASS_PATH,scala-compiler-2.11.6.jar) scala.tools.nsc.Main)))
+
+# make jar, $1 - .jar target
 JAR_LD1  = $(call SUPRESS,JAR,$1)$(JARC) $(JRFLAGS) -cf$(if $(MANIFEST),m) $(ospath) $(call ospath,$(MANIFEST)) -C $(call \
-            ospath,$(OBJDIR)) . $(call FORM_JAR_BUNDLES,$(BUNDLES))$(DEL_ON_FAIL)
-JAR_LD   = $(call JAVA_CC,$(if $(filter $(JARS) $(EXTJARS),$?),$(JSRC),$(filter $(JSRC),$?)))$(JAR_LD1)
+            ospath,$(OBJDIR)) . $(call FORM_JAR_BUNDLES,$(ALL_BUNDLES))$(DEL_ON_FAIL)
+
+JAR_LD   = $(if $(filter $(JARS) $(EXTJARS),$?),$(call SCALA_CC,$(SCALA),$(JSRC))$(call JAVA_CC,$(JSRC)),\
+            $(call SCALA_CC,$(filter $(SCALA),$?),$(JSRC))$(call JAVA_CC,$(filter $(JSRC),$?)))$(JAR_LD1)
 
 # $1 - target file: $(call FORM_JTRG,JAR)
 # $2 - sources:     $(call FIXPATH,$(JSRC))
-# $3 - manifest:    $(call FIXPATH,$(MANIFEST))
-# $4 - objdir:      $(call FORM_OBJ_DIR,JAR)
-# $5 - jars:        $(addprefix $(BIN_DIR)/,$(addsuffix .jar,$(JARS)))
+# $3 - sources:     $(call FIXPATH,$(SCALA))
+# $4 - manifest:    $(call FIXPATH,$(MANIFEST))
+# $5 - objdir:      $(call FORM_OBJ_DIR,JAR)
+# $6 - jars:        $(addprefix $(BIN_DIR)/,$(addsuffix .jar,$(JARS)))
 define JAR_TEMPLATE
-NEEDED_DIRS += $4
+NEEDED_DIRS += $5
 $(call STD_TARGET_VARS,$1)
-$1: JSRC      := $2
-$1: MANIFEST  := $3
-$1: OBJDIR    := $4
-$1: JARS      := $5
-$1: EXTJARS   := $(EXTJARS)
-$1: CLASSPATH := $(CLASSPATH)
-$1: BUNDLES   := $(BUNDLES)
-$1: VPREFIX   := $(VPREFIX)
-$1: JCFLAGS   := $(JCFLAGS)
-$1: JRFLAGS   := $(JRFLAGS)
-$1: $(EXTJARS) $5 $2 $3 | $(BIN_DIR) $4 $(ORDER_DEPS)
-	$$(eval COMMANDS := $(subst $$,$$$$,$(JARACTIONS)))$$(COMMANDS)$$(call JAR_LD,$$@)
+$1: JSRC         := $2
+$1: SCALA        := $3
+$1: MANIFEST     := $4
+$1: OBJDIR       := $5
+$1: JARS         := $6
+$1: EXTJARS      := $(EXTJARS)
+$1: CLASSPATH    := $(CLASSPATH)
+$1: ALL_BUNDLES  := $(BUNDLES) $(BUNDLE_FILES)
+$1: VPREFIX      := $(VPREFIX)
+$1: SCALAC       := $(SCALAC)
+$1: JAVAC_FLAGS  := $(JAVAC_FLAGS)
+$1: JRFLAGS      := $(JRFLAGS)
+$1: $(EXTJARS) $6 $2 $3 $4 $(call MAKE_BUNDLE_DEPS,$(BUNDLE_FILES)) | $(BIN_DIR) $5 $(ORDER_DEPS)
+	$$(eval $1: COMMANDS := $(subst $$,$$$$,$(JARACTIONS)))$$(COMMANDS)$$(call JAR_LD,$$@)
 $(CURRENT_MAKEFILE_TM): $1
-CLEAN += $4 $1
+CLEAN += $5 $1
 endef
 
 # how to build .jar library template
-JAR_RULES = $(if $(JAR),$(call JAR_TEMPLATE,$(call FORM_JTRG,JAR),$(call FIXPATH,$(JSRC)),$(call \
+JAR_RULES = $(if $(JAR),$(call JAR_TEMPLATE,$(call FORM_JTRG,JAR),$(call FIXPATH,$(JSRC)),$(call FIXPATH,$(SCALA)),$(call \
   FIXPATH,$(MANIFEST)),$(call FORM_OBJ_DIR,JAR),$(addprefix $(BIN_DIR)/,$(addsuffix .jar,$(JARS)))))
 
 # this file normally included at end of target Makefile
@@ -83,17 +98,20 @@ DEFINE_JTARGETS = $(if $(DEFINE_JTARGETS_EVAL),)
 
 # code to be called at beginning of target makefile
 define PREPARE_JVARS
-JAR         :=
-JSRC        :=
-JARS        :=
-EXTJARS     :=
-CLASSPATH   :=
-BUNDLES     :=
-MANIFEST    :=
-JCFLAGS     :=
-JRFLAGS     :=
-JARACTIONS  :=
-JAREXT      := .jar
+JAR          :=
+JSRC         :=
+SCALA        :=
+JARS         :=
+EXTJARS      :=
+CLASSPATH    :=
+BUNDLES      :=
+BUNDLE_FILES :=
+MANIFEST     :=
+SCALAC       :=
+JAVAC_FLAGS  :=
+JRFLAGS      :=
+JARACTIONS   :=
+JAREXT       := .jar
 endef
 
 # increment MAKE_CONT, eval tail code with $(DEFINE_JTARGETS)
