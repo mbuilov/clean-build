58c91eda439f38398a65e5eab222b0edf9215a4e
Michael Builov <mbuilov@gmail.com>
2015-12-15 01:05:02 +0400

    remove trace code

--- functions.mk
+++ functions.mk
@@ -10,25 +10,15 @@ $(empty)
 endef
 newline := $(newline)
 
-# run via $(MAKE) T=1 to trace functions
-ifeq ("$(origin T)","command line")
-TRACE := $T
-endif
-
-# 0 -> $(empty)
-TRACE := $(TRACE:0=)
-
-ifdef TRACE
-
 # print result $2 of the function $1
 infofn = $(info $1: result: =>$(newline)$2)$2
 
-# dump variables from list $1, prefixing output with optional prefix $2, $3 - optional pre-prefix example:
+# dump variables from list $1, prefixing output with optional prefix $2, $3 - optional pre-prefix, example:
 # $(call dump,VAR1,pr) -> print 'dump: pr:VAR1=xxx => yyy'
 dump = $(foreach v,$1,$(info $3dump: $(addsuffix : ,$2)$v=$(value $v)$(if $(filter recursive,$(flavor $v)), =>$(newline)$($v))))
 
 # trace function call parameters - print function name and parameter values
-# $(trace_params) must be the first statement of traced function body, for example: fun = $(trace_params)fn_body
+# add $(trace_params) as the first statement of traced function body, for example: fun = $(trace_params)fn_body
 trace_params = $(info params: $$($0) {)$(if $1,$(info $$1=$1))$(if $2,$(info $$2=$2))$(if $3,$(info $$3=$3))$(if \
   $4,$(info $$4=$4))$(if $5,$(info $$5=$5))$(if $6,$(info $$6=$6))$(if $7,$(info $$7=$7))$(if \
   $8,$(info $$8=$8))$(if $9,$(info $$9=$9))$(if $(10),$(info $$10=$(10)))$(if $(11),$(info $$11=$(11)))$(if \
@@ -59,9 +49,8 @@ endef
 # $1 - macro names
 # $2 - names of variables to dump before traced call
 # $3 - names of variables to dump after traced call
-trace_calls = $(eval $(foreach f,$1,$(call trace_calls_template,$f,$2)))
-
-endif # TRACE
+# note: may also be used for simple variables, for example: $(call trace_calls,Macro,VarPre,VarPost)
+trace_calls = $(eval $(foreach f,$1,$(call trace_calls_template,$f,$2,$3)))
 
 # replace spaces with ?
 unspaces = $(subst $(space),?,$1)
@@ -101,8 +90,8 @@ padto = $(call padto1,$(repl1))
 # call function $1 many times with arguments from list $2 groupped by $3 elements
 # and with auxiliary argument $4, separating function calls with $5
 xargs = $(call $1,$(wordlist 1,$3,$2),$4)$(if \
-  $(word $(words 1 $(wordlist 1,$3,$2)),$2),$5$(call \
-  xargs,$1,$(wordlist $(words 1 $(wordlist 1,$3,$2)),$(words $2),$2),$3,$4,$5))
+  $(word $(words x $(wordlist 1,$3,$2)),$2),$5$(call \
+  xargs,$1,$(wordlist $(words x $(wordlist 1,$3,$2)),$(words $2),$2),$3,$4,$5))
 
 # assuming that function $1($(sublist $2),$4) will return shell command
 # generate many shell commands separated by $(newline) - each command will be executed in new subshell
@@ -148,9 +137,6 @@ relpath = $(call relpath1,$(1:/=)/,$(2:/=)/)
 # join elements of list $1 with $2
 # example: $(call join_with,a b c,|) -> a|b|c
 join_with = $(patsubst %$2,%,$(subst $(space),,$(foreach x,$1,$x$2)))
-
-# trace calls to next functions if TRACE defined
-$(call trace_calls,qpath xcmd normp relpath join_with)
 
 # protect variables from modification in target makefiles
 CLEAN_BUILD_PROTECTED += empty space tab comma newline \
