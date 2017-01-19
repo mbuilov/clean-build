979c83e21bbfb8d675e02c2e8ecce5a5f441b0bc
Michael Builov <mbuilov@gmail.com>
2017-01-19 17:54:15 +0300

    initial release

--- LINUX/pc.mk
+++ LINUX/pc.mk
@@ -4,60 +4,55 @@
 # Licensed under GPLv2+, see COPYING
 #----------------------------------------------------------------------------------
 
-# this file included by $(MTOP)/LINUX/c.mk
+# pkg-config file generation
 
-# define standard pkg-config values
-INST_RPATH_DIR := $(patsubst %/,%,$(dir $(INST_RPATH)))
-PC_PREFIX      ?= $(firstword $(INST_PATH) $(INST_RPATH_DIR) /usr/local)
-PC_EXEC_PREFIX ?= $(if $(filter $(INST_RPATH_DIR),$(PC_PREFIX)),$${prefix},$(firstword $(INST_RPATH_DIR) $${prefix}))
-PC_LIBDIR      ?= $(if $(filter $(INST_RPATH_DIR),$(PC_EXEC_PREFIX)$(if \
-  $(filter $${prefix},$(PC_EXEC_PREFIX)), $(PC_PREFIX))),$${exec_prefix}/$(notdir \
-  $(INST_RPATH)),$(firstword $(INST_RPATH) $${exec_prefix}/lib))
+# example:
 
+define PC_EXAMPLE
+#
+# Author: John Smith
+#
+# This file has been put into the public domain.
+# You can do whatever you want with this file.
+#
 
+prefix=/usr/local
+exec_prefix=/usr/local
+libdir=/usr/local/lib
+includedir=/usr/local/include
 
-
-# note: some of WIN_RC_... variables may be already defined in $(TOP)/make/project.mk
-WIN_RC_PRODUCT_VERSION_MAJOR ?= PRODUCT_VERSION_MAJOR
-WIN_RC_PRODUCT_VERSION_MINOR ?= PRODUCT_VERSION_MINOR
-WIN_RC_PRODUCT_BUILD_NUM     ?= PRODUCT_BUILD_NUM
-WIN_RC_COMMENTS              ?= PRODUCT_TARGET "/" PRODUCT_OS "/" $(if $(filter DRV,$1),PRODUCT_KCPU,PRODUCT_UCPU) "/" PRODUCT_BUILD_DATE
-WIN_RC_COMPANY_NAME          ?= VENDOR_NAME
-WIN_RC_FILE_DESCRIPTION      ?= "$(GET_TARGET_NAME)"
-WIN_RC_FILE_VERSION          ?= PRODUCT_BUILD_VERSION
-WIN_RC_INTERNAL_NAME         ?= "$(GET_TARGET_NAME)"
-WIN_RC_LEGAL_COPYRIGHT       ?= VENDOR_COPYRIGHT
-WIN_RC_LEGAL_TRADEMARKS      ?=
-WIN_RC_PRIVATE_BUILD         ?=
-WIN_RC_PRODUCT_NAME          ?= PRODUCT_NAME
-WIN_RC_PRODUCT_VERSION       ?= PRODUCT_VERSION
-WIN_RC_LANG                  ?= 0409
-WIN_RC_CHARSET               ?= 04b0
+Name: libmylib
+Description: General purpose library
+URL: http://aa.bb.cc.dd.ee.com
+Version: 1.2.3
+Cflags: -I${includedir}
+Libs: -L${libdir} -lmylib
+Libs.private: -pthread
+endef
 
 # $1    - library comment (author, description, etc.)
 # $2    - ${prefix}
 # $3    - ${exec_prefix}
 # $4    - ${libdir}
 # $5    - ${includedir}
-# $6    - Name              $(DLL)
+# $6    - Name
 # $7    - Description
 # $8    - URL
-# $9    - Version            $(SOVER)
+# $9    - Version
 # $(10) - Requires
 # $(11) - Requires.private
 # $(12) - Conflicts
 # $(13) - CFLAGS
 # $(14) - dependent libraries
 # $(15) - private libs
-# note: PKGCONFIG_TEXT_TEMPLATE may be already defined in $(TOP)/make/project.mk
-ifndef PKGCONFIG_TEXT_TEMPLATE
-define PKGCONFIG_TEXT_TEMPLATE
-# $(subst $(newline),$(newline)# ,$1)
-
-prefix=$(firstword $2 /usr/local)
-exec_prefix=$(firstword $3 $${prefix})
-libdir=$(firstword $4 $${exec_prefix}/lib)
-includedir=$(firstword $5 $${prefix}/include)
+# note: PKGCONFIG_TEMPLATE may be already defined in $(TOP)/make/project.mk
+ifndef PKGCONFIG_TEMPLATE
+define PKGCONFIG_TEMPLATE
+$(if $1,# $(subst $(newline),$(newline)# ,$1)$(newline))
+prefix=$2
+exec_prefix=$3
+libdir=$4
+includedir=$5
 
 Name: $6$(if \
 $7,$(newline)Description: $7)$(if \
@@ -65,37 +60,51 @@ $8,$(newline)URL: $8)$(if \
 $9,$(newline)Version: $9)$(if \
 $(10),$(newline)Requires: $(10))$(if \
 $(11),$(newline)Requires.private: $(11))$(if \
-$(12),$(newline)Conflicts: $(12))
-Cflags: $(if $(13),$(13),-I$${includedir})
-Libs: $(if $(14),$(14),-L$${libdir} -l$6)$(if \
-$(15),Libs.private: $(15))
-endef # PKGCONFIG_TEXT_TEMPLATE
+$(12),$(newline)Conflicts: $(12))$(if \
+$(13),$(newline)Cflags: $(13))$(if \
+$(14),$(newline)Libs: $(14),$(14))$(if \
+$(15),$(newline)Libs.private: $(15))
+endef # PKGCONFIG_TEMPLATE
+endif
 
-# rule to generate pkg-config file for dynamic or static library
-# $1 - DLL or LIB
-define PKGCONFIG_TEMPLATE
-ifndef CB_TOOL_MODE
-ifndef DONT_GEN_PC
-TRG_PC := $(LIB_DIR)/lib$(DLL).pc
-$$(TRG_PC): | $(LIB_DIR)
-	$$(call SUP,GEN,$$@)$$(call ECHO,$$(call PKGCONFIG_TEXT_TEMPLATE,$(DLL),$(strip \
-$(PC_PREFIX)),$(strip \
-$(PC_EXEC_PREFIX)),$(strip \
-$(PC_INCLUDEDIR)),$(strip \
-$(PC_LIBDIR)),$(strip \
-$(PC_CFLAGS)),$(strip \
-$(PC_LIBS))) > $$@
-$$(call STD_TARGET_VARS,$$(TRG_PC))
-endif
-endif
+# generate pkg-config file for dynamic or static library
+# arguments - the same as for $(PKGCONFIG_TEMPLATE)
+define PKGCONFIG_GEN_RULE1
+$1: TEMPLATE := $(subst $(comment),$$$$(comment),$(subst $(newline),$$$$(newline),$(subst $$,$$$$,$2)))
+$1: | $(LIB_DIR)
+	$$(call SUP,GEN,$$@)$$(call ECHO,$$(subst $$$$(comment),$$(comment),$$(subst $$$$(newline),$$(newline),$$(TEMPLATE)))) > $$@
+$(call STD_TARGET_VARS,$1)
 endef
+PKGCONFIG_GEN_RULE = $(eval $(call PKGCONFIG_GEN_RULE1,$(LIB_DIR)/lib$6.pc,$(PKGCONFIG_TEMPLATE)))
+
+# define standard pkg-config values
+# note: some of PC_... variables may be already defined in $(TOP)/make/project.mk
+INST_RPATH_DIR := $(patsubst %/,%,$(dir $(INST_RPATH)))
+PC_PREFIX      ?= $(firstword $(INST_PATH) $(INST_RPATH_DIR) /usr/local)
+PC_EXEC_PREFIX ?= $(if $(filter $(INST_RPATH_DIR),$(PC_PREFIX)),$${prefix},$(firstword $(INST_RPATH_DIR) $${prefix}))
+PC_LIBDIR      ?= $(if $(filter $(INST_RPATH_DIR),$(PC_EXEC_PREFIX)$(if \
+  $(filter $${prefix},$(PC_EXEC_PREFIX)), $(PC_PREFIX))),$${exec_prefix}/$(notdir \
+  $(INST_RPATH)),$(firstword $(INST_RPATH) $${exec_prefix}/lib))
+PC_INCLUDEDIR  ?= $${prefix}/include
+PC_CFLAGS      ?= -I$${includedir}
+PC_LIBS        ?= -L$${libdir}
+
+# rule for generation of .pc-file using $(PC_...) predefined values
+# $1    - library name
+# $2    - library description
+# $3    - library version (for a DLL: $(SOVER))
+# $4    - library comment (author, description, etc.)
+# $5    - project url     (likely $(VENDOR_URL))
+# $6    - Requires section
+# $7    - Requires.private section
+# $8    - Conflicts section
+# $9    - additional CFLAGS
+# $(10) - Libs section
+# $(11) - Libs.private section
+PKGCONFIG_RULE = $(call PKGCONFIG_GEN_RULE,$4,$(PC_PREFIX),$(PC_EXEC_PREFIX),$(strip \
+$(PC_LIBDIR)),$(PC_INCLUDEDIR),$1,$2,$5,$3,$6,$7,$8,$(PC_CFLAGS) -l$1$(if $9, $9),$(10),$(11))
 
 # protect variables from modifications in target makefiles
 $(call CLEAN_BUILD_PROTECT_VARS, \
-  RC_OS RC_FT RC_FST \
-  WIN_RC_PRODUCT_DEFS_HEADER \
-  WIN_RC_PRODUCT_VERSION_MAJOR WIN_RC_PRODUCT_VERSION_MINOR WIN_RC_PRODUCT_BUILD_NUM \
-  WIN_RC_COMMENTS WIN_RC_COMPANY_NAME WIN_RC_FILE_DESCRIPTION WIN_RC_FILE_VERSION WIN_RC_INTERNAL_NAME \
-  WIN_RC_LEGAL_COPYRIGHT WIN_RC_LEGAL_TRADEMARKS WIN_RC_PRIVATE_BUILD \
-  WIN_RC_PRODUCT_NAME WIN_RC_PRODUCT_VERSION WIN_RC_SPECIAL_BUILD WIN_RC_LANG WIN_RC_CHARSET \
-  STD_VERSION_RC_TEMPLATE STD_RES_TEMPLATE1 ADD_RES_TEMPLATE STD_RES_TEMPLATE)
+  PKGCONFIG_TEMPLATE PKGCONFIG_GEN_RULE1 PKGCONFIG_GEN_RULE INST_RPATH_DIR \
+  PC_PREFIX PC_EXEC_PREFIX PC_LIBDIR PC_INCLUDEDIR PC_CFLAGS PC_LIBS PKGCONFIG_RULE)
