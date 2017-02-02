d8a3a43ce1b02dfd29811d0c325bdb5c83f97998
Michael Builov <mbuilov@s-terra.com>
2017-02-02 14:08:36 +0300

    environment for build with russian-localized Visual Studio

---
+++ WINXX/cp1251.bat
@@ -0,0 +1,4 @@
+rem prepare to build with russian-localized Visual Studio
+chcp 1251
+set INCLUDING_FILE_PATTERN=ֿנטלוקאםטו: גךכ‏קוםטו פאיכא:
+set LINKER_STRIP_STRINGS=.‏קפנ‎רץ ת‏פנ .‏קפנ‎רץ ת‏פנ קנעץ¨°ץ‎‏
