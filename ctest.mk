18417abe6ba67b72f0312a00da389825446d9466
Michael Builov <mbuilov@gmail.com>
2017-02-16 13:58:24 +0300

    fix bug: allow to run varianted executable

--- ctest.mk
+++ ctest.mk
@@ -12,16 +12,17 @@ ifndef DO_TEST_EXE_TEMPLATE
 
 # run $(EXE) and dump its stderr to $(EXE).out
 # $1 - auxiliary parameters to pass to executable
+# $2 - executable name
 define DO_TEST_EXE_TEMPLATE
-$(call ADD_GENERATED,$(BIN_DIR)/$(EXE).out)
-$(BIN_DIR)/$(EXE).out: $(call FORM_TRG,EXE)
+$(call ADD_GENERATED,$(BIN_DIR)/$2.out)
+$(BIN_DIR)/$2.out: $(call FORM_TRG,EXE)
 	$$(call SUP,TEST,$$@)$$< $1 2> $$@
 endef
 
 # for 'check' target, run executable
 # $1 - auxiliary parameters to pass to executable
 ifneq ($(filter check,$(MAKECMDGOALS)),)
-DO_TEST_EXE ?= $(eval $(DO_TEST_EXE_TEMPLATE))
+DO_TEST_EXE ?= $(eval $(call DO_TEST_EXE_TEMPLATE,$1,$(call GET_TARGET_NAME,EXE)))
 endif
 
 # protect variables from modifications in target makefiles
