7c7d0e7fba1125c60731838a6b285550435f1b8f
Michael Builov <mbuilov@gmail.com>
2017-02-16 13:51:59 +0300

    add comments

--- examples/hello/version/version.mk
+++ examples/hello/version/version.mk
@@ -3,8 +3,8 @@ include $(MTOP)/defs.mk
 GENERATED := $(GEN_DIR)/$(PRODUCT_NAMES_H)
 $(call ADD_GENERATED,$(GENERATED))
 
-# $$1 - $(word 1,$(BUILDNUMBERS))
-# $$2 - $(subst /,-,$(word 2,$(BUILDNUMBERS))) $(word 3,$(BUILDNUMBERS))
+# <build_num>  - $(firstword $(BUILDNUMBERS))
+# <build_date> - $(subst /,-,$(word 2,$(BUILDNUMBERS))) $(word 3,$(BUILDNUMBERS))
 define PRODUCT_NAMES_TEMPLATE
 #ifndef $(call toupper,$(subst .,_,$(PRODUCT_NAMES_H)))_INCLUDED
 #define $(call toupper,$(subst .,_,$(PRODUCT_NAMES_H)))_INCLUDED
@@ -16,11 +16,12 @@ define PRODUCT_NAMES_TEMPLATE
 #define PRODUCT_OS            "$(OS)"
 #define PRODUCT_UCPU          "$(CPU)"
 #define PRODUCT_TARGET        "$(TARGET)"
-#define PRODUCT_BUILD_NUM     $$1
-#define PRODUCT_BUILD_DATE    "$$2"
+#define PRODUCT_BUILD_NUM     <build_num>
+#define PRODUCT_BUILD_DATE    "<build_date>"
 #endif
 endef
 
+# PRODUCT_BUILDNUMBERS may be defined as: 17160 2017/02/16 10:43:09
 ifndef PRODUCT_BUILDNUMBERS
 $(GENERATED): $(call GET_TOOL,buildnumber)
 endif
@@ -30,7 +31,7 @@ $(GENERATED): BUILDNUMBERS := $(PRODUCT_BUILDNUMBERS)
 $(GENERATED):
 	$(if $(BUILDNUMBERS),,$(eval $@: BUILDNUMBERS := $(shell $(call ospath,$(call GET_TOOL,buildnumber)))))
 	$(if $(BUILDNUMBERS),,$(error PRODUCT_BUILDNUMBERS not defined))
-	$(call SUP,GEN,$@)$(call ECHO,$(subst $$2,$(subst /,-,$(word 2,$(BUILDNUMBERS))) $(word \
-  3,$(BUILDNUMBERS)),$(subst $$1,$(word 1,$(BUILDNUMBERS)),$(TEMPL)))) > $@
+	$(call SUP,GEN,$@)$(call ECHO,$(subst <build_date>,$(subst /,-,$(word 2,$(BUILDNUMBERS))) $(word \
+  3,$(BUILDNUMBERS)),$(subst <build_num>,$(firstword $(BUILDNUMBERS)),$(TEMPL)))) > $@
 
 $(DEFINE_TARGETS)
