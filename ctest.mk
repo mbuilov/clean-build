f0d6f3238018bec450e82db5410d639b93cb8dd4
Michael Builov <mbuilov@gmail.com>
2017-03-06 13:17:56 +0300

    create simlinks for tests as needed

--- ctest.mk
+++ ctest.mk
@@ -24,18 +24,25 @@ endef
 # $1 - $(call FORM_TRG,EXE,$v)
 # $2 - built shared libraries needed by executable, in form <library_name>.<major_number>
 ifeq (UNIX,$(OSTYPE))
-TEST_EXE_SOFTLINKS ?= $(if $2,$1: | $(addprefix $(LIB_DIR)/$(DLL_PREFIX),$(subst .,$(DLL_SUFFIX).,$2)))
+TEST_EXE_SOFTLINKS ?= $(if $2,$1: | $(addprefix $(LIB_DIR)/$(DLL_PREFIX),$(subst .,$(DLL_SUFFIX).,$2))$(call TEST_NEED_SIMLINKS,$2))
 endif
 
 # $1 - $(LIB_DIR)/$(DLL_PREFIX)$(subst .,$(DLL_SUFFIX).,$d)
 # $2 - $(DLL_PREFIX)<library_name>$(DLL_SUFFIX)
 # $d - built shared library in form <library_name>.<major_number>
 define SO_SOFTLINK_TEMPLATE
-$(empty)
 $1: | $(LIB_DIR)/$2
 	$$(call SUP,LN,$$@)$$(call LN,$2,$$@)
 $(TOCLEAN)
+CB_GENERATED_SIMLINK_RULES += $d
 endef
+
+# $1 - built shared libraries needed by executable, in form <library_name>.<major_number>
+ifeq (UNIX,$(OSTYPE))
+CB_GENERATED_SIMLINK_RULES:=
+TEST_NEED_SIMLINKS ?= $(foreach d,$1,$(if $(filter $d,$(CB_GENERATED_SIMLINK_RULES)),,$(eval $(call \
+  SO_SOFTLINK_TEMPLATE,$(LIB_DIR)/$(DLL_PREFIX)$(subst .,$(DLL_SUFFIX).,$d),$(DLL_PREFIX)$(firstword $(subst ., ,$d))$(DLL_SUFFIX)))))
+endif
 
 ifneq ($(filter check clean,$(MAKECMDGOALS)),)
 
@@ -44,16 +51,9 @@ ifneq ($(filter check clean,$(MAKECMDGOALS)),)
 # $2 - auxiliary parameters to pass to executable
 DO_TEST_EXE ?= $(eval $(foreach v,$(call GET_VARIANTS,EXE),$(newline)$(call DO_TEST_EXE_TEMPLATE,$(call FORM_TRG,EXE,$v),$1,$2)))
 
-# for 'check' target, create runtime simlinks to shared libraries so dynamic linker will find them
-# $1 - list of built shared libraries in form: <library_name>.<major_number>
-ifeq (UNIX,$(OSTYPE))
-TEST_NEED_SIMLINKS ?= $(eval $(foreach d,$1,$(call SO_SOFTLINK_TEMPLATE,$(LIB_DIR)/$(DLL_PREFIX)$(subst \
-  .,$(DLL_SUFFIX).,$d),$(DLL_PREFIX)$(firstword $(subst ., ,$d))$(DLL_SUFFIX))))
-endif
-
 endif # check
 
 # protect variables from modifications in target makefiles
-$(call CLEAN_BUILD_PROTECT_VARS,DO_TEST_EXE_TEMPLATE TEST_EXE_SOFTLINKS SO_SOFTLINK_TEMPLATE DO_TEST_EXE TEST_NEED_SIMLINKS)
+$(call CLEAN_BUILD_PROTECT_VARS,DO_TEST_EXE_TEMPLATE TEST_EXE_SOFTLINKS SO_SOFTLINK_TEMPLATE TEST_NEED_SIMLINKS DO_TEST_EXE)
 
 endif # DO_TEST_EXE_TEMPLATE
