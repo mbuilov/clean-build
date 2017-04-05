65c463480bdacceae99744357f9a1b115b7ec2ec
Michael Builov <mbuilov@gmail.com>
2017-04-05 13:43:14 +0300

    fix template

--- exts/jtest.mk
+++ exts/jtest.mk
@@ -12,29 +12,25 @@ ifndef DO_TEST_JAR_TEMPLATE
 
 # run $(JAR) and dump its stderr to $(JAR).out
 # $1 - $(call FORM_JTRG,JAR)
-# $2 - auxiliary parameters to pass to executed jar
-# $3 - additional class paths needed to run executed jar
-# $4 - options to pass to $(JAVA)
+# $2 - main class name, for example com.mycomp.myproj.MyMainClass
+# $3 - $(call FORM_BUILT_JARS,$(JARS)) + class paths/jars needed to run the test
+# $4 - auxiliary parameters to pass to executed jar
+# $5 - options to pass to $(JAVA) interpreter
 # note: $(JAVA) - path to java interpreter must be defined
 define DO_TEST_JAR_TEMPLATE
 $(call ADD_GENERATED,$1.out)
 $1.out: $1
-	$$(call SUP,TEST,$$@)$(JAVA) $(call FORM_CLASS_PATH,$3) $4 -jar $$(call jpath,$$<) $2 > $$@
+	$$(call SUP,TEST,$$@)$(JAVA) $5 $$(call FORM_CLASS_PATH,$3 $$<) $2 $4 > $$@
 endef
-
-# $1 - main class name
-# $2 - list of jars
-JTEST_MANIFEST ?= $(if $2,Class-Path: $2$(newline))Main-Class: $1
 
 ifneq ($(filter check clean,$(MAKECMDGOALS)),)
 
 # for 'check' target, run built jar(s)
-# $1 - auxiliary parameters to pass to executed jar
-# $2 - main class name
-# $3 - list of jars
-# $2 - options to pass to $(JAVA)
-# $3 - additional class paths needed to run executed jar
-DO_TEST_JAR ?= $(eval $(call DO_TEST_JAR_TEMPLATE,$(call FORM_JTRG,JAR),$1,$2,$3))
+# $1 - main class name, for example com.mycomp.myproj.MyMainClass
+# $2 - class paths/jars needed to run the test
+# $3 - auxiliary parameters to pass to executed jar
+# $4 - options to pass to $(JAVA) interpreter
+DO_TEST_JAR ?= $(eval $(call DO_TEST_JAR_TEMPLATE,$(call FORM_JTRG,JAR),$1,$(call FORM_BUILT_JARS,$(JARS)) $2,$3,$4))
 
 endif # check
 
