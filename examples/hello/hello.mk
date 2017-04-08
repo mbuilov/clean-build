aa3c461c06696dab79d92ec2b380ed4a7d11f421
Michael Builov <mbuilov@gmail.com>
2017-04-08 04:46:08 +0400

    build fix

--- examples/hello/hello.mk
+++ examples/hello/hello.mk
@@ -1,11 +1,15 @@
-# to build C/C++
+# include templates for building C/C++ sources
 include $(MTOP)/c.mk
-# definitions for running built executable
-include $(MTOP)/ctest.mk
+
+# add definitions for running built executable
+include $(MTOP)/exts/ctest.mk
+
 # we will build S-variant of 'hello' executable - with statically linked C runtime
 EXE := hello S
 SRC := hello.c
+
 # will run built executable
 $(DO_TEST_EXE)
+
 # define targets and rules how to build them
 $(DEFINE_TARGETS)
