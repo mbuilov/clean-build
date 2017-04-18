1ac64035cfc556174cda09a09f5c3fb5a4e1d44f
Michael Builov <mbuilov@gmail.com>
2017-04-18 15:41:19 +0300

    send output of rm, mkdir, cp, ln and chmod to stderr

--- LINUX/tools.mk
+++ LINUX/tools.mk
@@ -6,18 +6,18 @@
 
 # this file included by $(MTOP)/defs.mk
 
-DEL   = rm -f$(if $(VERBOSE),v) $1
-RM    = $(if $(VERBOSE),,@)rm -rf$(if $(VERBOSE),v) $1
-MKDIR = mkdir -p$(if $(VERBOSE),v) $1
+DEL   = rm -f$(if $(VERBOSE),v) $1 >&2
+RM    = $(if $(VERBOSE),,@)rm -rf$(if $(VERBOSE),v) $1 >&2
+MKDIR = mkdir -p$(if $(VERBOSE),v) $1 >&2
 SED  := sed
 SED_EXPR = '$1'
 CAT   = cat $1
 ECHO  = printf '$(subst ','"'"',$(subst $(newline),\n,$(subst \,\\,$(subst %,%%,$1))))\n'
 NUL  := /dev/null
-CP    = cp$(if $(VERBOSE), -v) $1 $2
-LN    = ln -sf$(if $(VERBOSE),v) $1 $2
+CP    = cp$(if $(VERBOSE), -v) $1 $2 >&2
+LN    = ln -sf$(if $(VERBOSE),v) $1 $2 >&2
 TOUCH = touch $1
-CHMOD = chmod$(if $(VERBOSE), -v) $1 $2
+CHMOD = chmod$(if $(VERBOSE), -v) $1 $2 >&2
 
 # execute command $2 in directory $1
 EXECIN = pushd $1 >/dev/null && { $2 && popd >/dev/null || { popd >/dev/null; false; } }
