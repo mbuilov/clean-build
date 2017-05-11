7c3f13b0ff77c61e7da3157bb12f31bb74b45066
Michael Builov <mbuilov@gmail.com>
2017-05-11 14:08:55 +0300

    do not override variables that normally must be set in project configuration makefile: PRODUCT_NAMES_H, VENDOR_NAME, PRODUCT_NAME and VENDOR_COPYRIGHT

--- examples/hello/conf.mk
+++ examples/hello/conf.mk
@@ -12,9 +12,9 @@ override MTOP := $(abspath $(dir $(lastword $(MAKEFILE_LIST)))../../)
 override PRODUCT_VER := 1.0.0
 
 # next variables are needed for generating resurce file under Windows
-override PRODUCT_NAMES_H  := product_names.h
-override VENDOR_NAME      := Michael M. Builov
-override PRODUCT_NAME     := Sample app
-override VENDOR_COPYRIGHT := Copyright (C) 2015-2017 Michael M. Builov, https://github.com/mbuilov/clean-build
+PRODUCT_NAMES_H  := product_names.h
+VENDOR_NAME      := Michael M. Builov
+PRODUCT_NAME     := Sample app
+VENDOR_COPYRIGHT := Copyright (C) 2015-2017 Michael M. Builov, https://github.com/mbuilov/clean-build
 
 endif
