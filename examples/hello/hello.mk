ac92b4a69546d5682c8f433d26aafc245ea14669
Michael Builov <mbuilov@gmail.com>
2017-05-15 23:57:28 +0400

    fix comments

--- examples/hello/hello.mk
+++ examples/hello/hello.mk
@@ -18,12 +18,13 @@ $(DO_TEST_EXE)
 $(call SET_MAKEFILE_INFO,hello)
 
 # if executable output exist - print it
-# note: $| - automatic variable - first order-dependency
-# note: pass 1 as third argument to SUP function to not update percents of executed makefiles
+# note: $| - automatic variable - first order-only dependency of the target
+# note: pass 1 as 4-th argument to SUP function to not update percents of executed makefiles
 hello: | $(call FORM_TRG,EXE,S).out
-	$(call SUP,CAT,$|,1)$(call CAT,$|) >&2
+	$(call SUP,CAT,$|,,1)$(call CAT,$|) >&2
 
-# to complete 'check' target, need to update 'hello' target
+# to complete 'check' goal, need to update 'hello'
+# note: 'check' goal is one of clean-build predefined goals
 check: hello
 
 # hello - is not a file, it is a PHONY target
