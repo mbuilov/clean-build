7d8942c53f146f1ece69e9d5baa920a6ede90eef
Michael Builov <mbuilov@gmail.com>
2017-05-18 12:15:10 +0300

    add comments, optimize DEL_ON_FAIL, CP now preserves modification time

--- SOLARIS/tools.mk
+++ SOLARIS/tools.mk
@@ -8,24 +8,56 @@
 
 OSTYPE := UNIX
 
-DEL   = rm -f $1
-RM    = $(QUIET)rm -rf $1
+# delete files $1
+DEL = rm -f $1
+
+# delete files and directories
+# note: do not need to add $(QUIET) before $(RM)
+RM = $(QUIET)rm -rf $1
+
+# to avoid races, MKDIR must be called only if destination directory does not exist
+# note: MKDIR should create intermediate parent directories of destination directory
 MKDIR = mkdir -p $1
-SED  := sed
+
+# stream-editor executable
+# note: SED value may be overridden either in command line or in project configuration file, like:
+# override SED := /usr/local/bin/sed
+SED := sed
+
+# escape command line argument to pass it to $(SED)
 SED_EXPR = '$(subst \n,\$(newline),$(subst \t,\$(tab),$1))'
-CAT   = cat $1
-ECHO  = printf '$(subst ','"'"',$(subst $(newline),\n,$(subst \,\\,$(subst %,%%,$1))))\n'
-NUL  := /dev/null
-CP    = cp $1 $2
-LN    = ln -sf $1 $2
+
+# print contents of given file (to stdout, for redirecting it to output file)
+CAT = cat $1
+
+# print lines of text (to stdout, for redirecting it to output file)
+# note: each line will be ended with LF
+#ECHO = printf '$(subst ','"'"',$(subst $(newline),\n,$(subst \,\\,$(subst %,%%,$1))))\n'
+
+# null device for redirecting output into
+NUL := /dev/null
+
+# copy preserving modification date, ownership and mode:
+# - file(s) $1 to directory $2 or
+# - file $1 to file $2
+CP = cp -p $1 $2
+
+# create symbolic link
+# note: this tool is UNIX-specific and may be not defined for other OSes
+LN = ln -sf $1 $2
+
+# update modification date of given file(s) or create file(s) if they do not exist
 TOUCH = touch $1
+
+# set mode $1 of given file(s) $2
+# note: this tool is UNIX-specific and may be not defined for other OSes
 CHMOD = chmod $1 $2
 
 # execute command $2 in directory $1
 EXECIN = pushd $1 >/dev/null && { $2 && popd >/dev/null || { popd >/dev/null; false; } }
 
-# delete target if failed to build it and exit shell with some error code
-DEL_ON_FAIL = || ($(DEL); false)
+# delete target file(s) if failed to build them and exit shell with error code 1
+DEL_ON_FAIL = || { $(DEL); false; }
 
 # protect variables from modifications in target makefiles
 $(call CLEAN_BUILD_PROTECT_VARS,DEL RM MKDIR SED SED_EXPR CAT ECHO EXECIN NUL CP LN TOUCH CHMOD DEL_ON_FAIL)
