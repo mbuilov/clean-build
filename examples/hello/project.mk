bac870861cb79a548e632c8205fd5e96c9033f7d
Michael Builov <mbuilov@gmail.com>
2017-05-19 17:34:55 +0300

    fix comments

--- examples/hello/project.mk
+++ examples/hello/project.mk
@@ -7,22 +7,37 @@ ifneq (override,$(origin TOP))
 override CLEAN_BUILD_REQUIRED_VERSION := 0.6.0
 
 # TOP - project root directory
-# use this variable to reference project files: sources, makefiles, include paths, etc.
+# define this variable for referencing project files: sources, makefiles, include paths, etc.
 override TOP := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
-# variable required by clean-build - path to built artifacts
-BUILD := $(TOP)/build
+# BUILD - variable required by clean-build - path to built artifacts
+#
+# Note: do not take BUILD from environment - reset it
+# Note: if BUILD is defined in make command line, it will not be reset
+BUILD:=
+
+ifndef BUILD
+# ok, BUILD was not specified in make command line, set default value
+override BUILD := $(TOP)/build
+endif
 
 # clean-build generated config file (optional)
-# note: distclean goal, defined by clean-build will delete $(BUILD), together with $(CONFIG_FILE)
+#
+# Note: generated config file will remember values of command-line or overridden variables, including BUILD variable.
+#  But, because config file is referenced via $(BUILD), to use non-default BUILD value,
+#  it must be specified in command line every time, for example:
+#  $ make BUILD:=/tmp
+#
+# Note: by completing 'distclean' goal, defined by clean-build, $(BUILD) directory will be deleted
+#  - together with $(CONFIG_FILE) generated under $(BUILD)
 override CONFIG_FILE := $(BUILD)/conf.mk
 
-# define variable - for referencing clean-build files in project makefiles
-# note: following general rules for working with variables,
-# this variable SHOULD NOT be taken from environment,
-# instead, it should be defined either in command line or
-# in generated config.mk (for example, included from project.mk)
-
+# define variable - for referencing clean-build files (defs.mk, parallel.mk, ...) in project makefiles
+#
+# Note: following general rules for working with variables,
+#  this variable SHOULD NOT be taken from environment, instead,
+#  it should be defined either in command line or in generated $(CONFIG_FILE)
+#
 # for this example MTOP may be defined automatically
 MTOP := $(abspath $(dir $(lastword $(MAKEFILE_LIST)))../../)
 
