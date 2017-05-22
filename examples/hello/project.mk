f071ff0722637aaeb7f1b07663c7a334efcd2fd8
Michael Builov <mbuilov@gmail.com>
2017-05-22 13:50:00 +0300

    simplify, clean-build starting with version 0.6.3 will use project-defined variables values over default ones

--- examples/hello/project.mk
+++ examples/hello/project.mk
@@ -3,33 +3,16 @@ ifneq (override,$(origin TOP))
 
 # project configuration file
 
-# specify version of clean-build build system required by this project
-# note: do not take CLEAN_BUILD_REQUIRED_VERSION value from environment
-CLEAN_BUILD_REQUIRED_VERSION:=
-
-# if CLEAN_BUILD_REQUIRED_VERSION is not specified in command line, set default value
-# note: CLEAN_BUILD_REQUIRED_VERSION must be overridden,
-# because clean-build defines it as CLEAN_BUILD_REQUIRED_VERSION := 0.0.0
-ifndef CLEAN_BUILD_REQUIRED_VERSION
-override CLEAN_BUILD_REQUIRED_VERSION := 0.6.0
-endif
-
 # TOP - project root directory
 # define this variable for referencing project files: sources, makefiles, include paths, etc.
-# note: override TOP value occasionally set in environment or in command line
 # note: TOP variable is not used by clean-build
 override TOP := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 
-# BUILD - variable required by clean-build - path to built artifacts
-# note: do not take BUILD from environment - set default value here
-BUILD:=
+# specify version of clean-build build system required by this project
+CLEAN_BUILD_REQUIRED_VERSION := 0.6.3
 
-# if BUILD is not specified in command line, set default value
-# note: BUILD must be overridden,
-# because clean-build defines it as BUILD:=
-ifndef BUILD
-override BUILD := $(TOP)/build
-endif
+# BUILD - variable required by clean-build - path to built artifacts
+BUILD := $(TOP)/build
 
 # clean-build generated config file (optional)
 #
@@ -41,22 +24,12 @@ endif
 # Note: by completing 'distclean' goal, defined by clean-build, $(BUILD) directory will be deleted
 #  - together with $(CONFIG_FILE), if it was generated under $(BUILD)
 #
-# Note: do not take CONFIG_FILE from environment
-CONFIG_FILE:=
-
-# if CONFIG_FILE is not specified in command line, set default value
-# note: CONFIG_FILE must be overridden,
-# because clean-build defines it as CONFIG_FILE:=
-ifndef CONFIG_FILE
-# note: define CONFIG_FILE as recursive variable - in case if BUILD is also recursive
+# note: define CONFIG_FILE as recursive variable - for the case when BUILD is defined in command line as recursive
 # note: clean-build will override CONFIG_FILE to make it non-recursive (simple)
-override CONFIG_FILE = $(BUILD)/conf.mk
-endif
+CONFIG_FILE = $(BUILD)/conf.mk
 
 # major.minor.patch
-# note: PRODUCT_VER must be overridden,
-# because clean-build defines it as PRODUCT_VER := 0.0.1
-override PRODUCT_VER := 1.0.0
+PRODUCT_VER := 1.0.0
 
 # next variables are needed for generating resource file under Windows
 PRODUCT_NAMES_H  := product_names.h
@@ -67,8 +40,8 @@ VENDOR_COPYRIGHT := Copyright (C) 2015-2017 Michael M. Builov, https://github.co
 # define variable - for referencing clean-build files (defs.mk, parallel.mk, ...) in project makefiles
 #
 # Note: because use of environment variables in makefiles is discouraged,
-#  this variable SHOULD NOT be taken from environment, instead,
-#  it should be defined either in command line or in generated $(CONFIG_FILE)
+#  this variable SHOULD NOT be taken from environment, instead, it should be defined
+#  in command line (and later it may be taken from generated $(CONFIG_FILE))
 #
 # Only for this example MTOP may be defined automatically
 # note: MTOP variable is not used by clean-build
