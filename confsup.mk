0c3c633667ee137882d73a40d38aabf2f76aec8e
Michael Builov <mbuilov@gmail.com>
2017-05-22 11:19:21 +0300

    do not include $(CONFIG_FILE) - it should be included in project.mk

--- confsup.mk
+++ confsup.mk
@@ -23,6 +23,10 @@ conf unconf: override CF := $(CONFIG_FILE)
 
 ifneq (,$(filter conf,$(MAKECMDGOALS)))
 
+ifneq (,$(filter unconf,$(MAKECMDGOALS)))
+$(error conf and unconf goals cannot be specified at the same time)
+endif
+
 # generate configuration file
 # note: SUP - defined in $(CLEAN_BUILD_DIR)/defs.mk
 # note: ECHO - defined in $(OSDIR)/$(OS)/tools.mk
@@ -37,21 +41,8 @@ ifneq (,$(filter $(abspath $(BUILD))/%,$(CONFIG_FILE)))
 NEEDED_DIRS += $(patsubst %/,%,$(dir $(CONFIG_FILE)))
 else
 $(patsubst %/,%,$(dir $(CONFIG_FILE))):
-	$(error config file directory $@ does not exist, it should be created manually)
+	$(error config file directory '$@' does not exist, it is not under '$(BUILD)', so should be created manually)
 endif
-
-endif # conf
-
-# source old definitions, if $(CONFIG_FILE) does exist
-# else reset CONFIG_FILE variable to mark that $(CONFIG_FILE) was not sourced
-ifeq (,$(wildcard $(CONFIG_FILE)))
-$(info warning: config file $(CONFIG_FILE) does not exist)
-override CONFIG_FILE:=
-else
-include $(CONFIG_FILE)
-endif
-
-ifneq (,$(filter conf,$(MAKECMDGOALS)))
 
 # use of environment variables is discouraged,
 # override variable only if it's not specified in command-line
@@ -68,22 +59,24 @@ $(if $(filter simple,$(flavor $v)),override $v:=$$(value $v))
 
 endef
 
-# generated $(CONFIG_FILE) may be already sourced,
+# generated $(CONFIG_FILE) is likely already sourced,
 # 1) override variables in $(CONFIG_FILE) with values specified in command line,
 # 2) save new variables specified in command line to $(CONFIG_FILE)
 # note: save variables current values in target-specific variable CONFIG_FILE_TEXT - variables may be overridden later
-# note: don't override GNUMAKEFLAGS, CLEAN_BUILD_VERSION and CONFIG_FILE variables by including $(CONFIG_FILE)
+# note: don't override GNUMAKEFLAGS, CLEAN_BUILD_VERSION, CONFIG_FILE and $(dump_max) variables by including $(CONFIG_FILE)
 conf: override CONFIG_FILE_TEXT := $(foreach v,$(filter-out \
   GNUMAKEFLAGS CLEAN_BUILD_VERSION CONFIG_FILE $(dump_max),$(.VARIABLES)),$(if \
   $(findstring "command line","$(origin $v)")$(findstring "override","$(origin $v)"),$(OVERRIDE_VAR_TEMPLATE)))
 
-endif # conf
+else ifneq (,$(filter unconf,$(MAKECMDGOALS)))
 
 # delete configuration file
 # note: DEL - defined in $(OSDIR)/$(OS)/tools.mk
 # note: pass 1 as 4-th argument of SUP function to not update percents of executed target makefiles
 unconf:
 	$(call SUP,RM,$(CF),,1)$(call DEL,$(CF))
+
+endif # unconf
 
 # conf/unconf - not a files
 .PHONY: conf unconf
