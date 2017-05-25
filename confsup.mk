8cbebdf0ee3651cc9acb740feba7241be7f159eb
Michael Builov <mbuilov@gmail.com>
2017-05-25 10:51:52 +0300

    rename: CONFIG_FILE -> CONFIG

--- confsup.mk
+++ confsup.mk
@@ -5,21 +5,21 @@
 #----------------------------------------------------------------------------------
 
 # configuration file generation:
-# if CONFIG_FILE is set, define conf/unconf goals
+# if CONFIG is set, define conf/unconf goals
 
-# do not inherit CONFIG_FILE from environment - if needed,
-# CONFIG_FILE may be specified before including this file via:
-# override CONFIG_FILE = $(BUILD)/conf.mk
-CONFIG_FILE:=
+# do not inherit CONFIG from environment - if needed,
+# CONFIG may be specified before including this file via:
+# override CONFIG = $(BUILD)/conf.mk
+CONFIG:=
 
-ifdef CONFIG_FILE
+ifdef CONFIG
 
-# CONFIG_FILE variable should be simple
-override CONFIG_FILE := $(abspath $(CONFIG_FILE))
+# CONFIG variable should be simple
+override CONFIG := $(abspath $(CONFIG))
 
-# save $(CONFIG_FILE) in target-specific variable CF
-# - to be safe if CONFIG_FILE get overridden
-conf unconf: override CF := $(CONFIG_FILE)
+# save $(CONFIG) in target-specific variable CF
+# - to be safe if CONFIG get overridden
+conf unconf: override CF := $(CONFIG)
 
 ifneq (,$(filter conf,$(MAKECMDGOALS)))
 
@@ -40,29 +40,29 @@ $(if $(filter simple,$(flavor $v)),override $v:=$$(value $v))
 endif
 endef
 
-# generated $(CONFIG_FILE) is likely already sourced,
-# 1) override variables in $(CONFIG_FILE) with values specified in command line,
-# 2) save new variables specified in command line to $(CONFIG_FILE)
-# note: save variables current values in target-specific variable CONFIG_FILE_TEXT - variables may be overridden later
-# note: don't override GNUMAKEFLAGS, CLEAN_BUILD_VERSION, CONFIG_FILE and $(dump_max) variables by including $(CONFIG_FILE)
-conf: override CONFIG_FILE_TEXT := $(foreach v,$(filter-out \
-  GNUMAKEFLAGS CLEAN_BUILD_VERSION CONFIG_FILE $(dump_max),$(.VARIABLES)),$(if \
+# generated $(CONFIG) file is likely already sourced,
+# 1) override variables in $(CONFIG) file with values specified in command line,
+# 2) save new variables specified in command line to $(CONFIG) file
+# note: save variables current values in target-specific variable CONFIG_TEXT - variables may be overridden later
+# note: don't override GNUMAKEFLAGS, CLEAN_BUILD_VERSION, CONFIG and $(dump_max) variables by including $(CONFIG) file
+conf: override CONFIG_TEXT := $(foreach v,$(filter-out \
+  GNUMAKEFLAGS CLEAN_BUILD_VERSION CONFIG $(dump_max),$(.VARIABLES)),$(if \
   $(findstring "command line","$(origin $v)")$(findstring "override","$(origin $v)"),$(OVERRIDE_VAR_TEMPLATE)))
 
 # generate configuration file
 # note: SUP - defined in $(CLEAN_BUILD_DIR)/defs.mk
 # note: WRITE - defined in $(OSDIR)/$(OS)/tools.mk
 # note: pass 1 as 4-th argument of SUP function to not update percents of executed target makefiles
-# note: CONFIG_FILE_TEXT is defined below
-conf:| $(patsubst %/,%,$(dir $(CONFIG_FILE)))
-	$(call SUP,GEN,$(CF),,1)$(call WRITE,$(CONFIG_FILE_TEXT),$(CF),10)
+# note: CONFIG_TEXT was defined above as target-specific variable
+conf:| $(patsubst %/,%,$(dir $(CONFIG)))
+	$(call SUP,GEN,$(CF),,1)$(call WRITE,$(CONFIG_TEXT),$(CF),10)
 
-# if $(CONFIG_FILE) is under $(BUILD), create config directory automatically
-# else - $(CONFIG_FILE) is outside of $(BUILD), config directory must be created manually
-ifneq (,$(filter $(abspath $(BUILD))/%,$(CONFIG_FILE)))
-NEEDED_DIRS += $(patsubst %/,%,$(dir $(CONFIG_FILE)))
+# if $(CONFIG) file is under $(BUILD), create config directory automatically
+# else - $(CONFIG) file is outside of $(BUILD), config directory must be created manually
+ifneq (,$(filter $(abspath $(BUILD))/%,$(CONFIG)))
+NEEDED_DIRS += $(patsubst %/,%,$(dir $(CONFIG)))
 else
-$(patsubst %/,%,$(dir $(CONFIG_FILE))):
+$(patsubst %/,%,$(dir $(CONFIG))):
 	$(error config file directory '$@' does not exist, it is not under '$(BUILD)', so should be created manually)
 endif
 
@@ -82,4 +82,4 @@ endif # unconf
 endif
 
 # protect variables from modification in target makefiles
-$(call CLEAN_BUILD_PROTECT_VARS,CONFIG_FILE)
+$(call CLEAN_BUILD_PROTECT_VARS,CONFIG)
