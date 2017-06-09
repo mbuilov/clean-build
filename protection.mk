fd0a9bcd7541500c6abf00b088c9aa54d1519bd9
Michael Builov <mbuilov@gmail.com>
2017-06-09 15:43:19 +0300

    add support for passing protected variables in form: AAA=b1,b2=e1,e2 BBB=b1,b2=e1,e2,...

--- protection.mk
+++ protection.mk
@@ -49,12 +49,15 @@ endef
 # NOTE: if CLEAN_BUILD_PROTECTED_VALUES_SAVED is not defined yet - then $(DEF_HEAD_CODE) was never executed yet:
 # - when it will be executed, it will save initial values of protected vars, so nothing to do here,
 # else - replace old values of protected vars with current ones
-define CLEAN_BUILD_PROTECT_VARS1
+define CLEAN_BUILD_PROTECT_VARS2
 CLEAN_BUILD_PROTECTED_VARS := $$(sort $$(CLEAN_BUILD_PROTECTED_VARS) $1)
 ifdef CLEAN_BUILD_PROTECTED_VALUES_SAVED
 $$(foreach x,CLEAN_BUILD_PROTECTED_VARS $1,$$(eval $$x.v? = $$(call CLEAN_BUILD_ENCODE_VAR_VALUE,$$x)))
 endif
 endef
+
+# list $1: AAA=b1,b2=e1,e2 BBB=b1,b2=e1,e2,...
+CLEAN_BUILD_PROTECT_VARS1 = $(call CLEAN_BUILD_PROTECT_VARS2,$(foreach v,$1,$(firstword $(subst =, ,$v))))
 CLEAN_BUILD_PROTECT_VARS = $(eval $(CLEAN_BUILD_PROTECT_VARS1))
 
 # macro to check if clean-build protected $x variable value was changed in target makefile
@@ -94,7 +97,7 @@ endef
 
 # protect variables from modifications in target makefiles
 CLEAN_BUILD_PROTECTED_VARS := CLEAN_BUILD_PROTECTED_VARS MCHECK CLEAN_BUILD_ENCODE_VAR_VALUE CLEAN_BUILD_CHECK_AT_HEAD \
-  CLEAN_BUILD_PROTECT_VARS1 CLEAN_BUILD_PROTECT_VARS CLEAN_BUILD_CHECK_PROTECTED_VAR CLEAN_BUILD_CHECK_AT_TAIL
+  CLEAN_BUILD_PROTECT_VARS2 CLEAN_BUILD_PROTECT_VARS1 CLEAN_BUILD_PROTECT_VARS CLEAN_BUILD_CHECK_PROTECTED_VAR CLEAN_BUILD_CHECK_AT_TAIL
 
 else # !MCHECK
 
