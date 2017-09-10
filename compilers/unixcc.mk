c64a1dfc057889f4ff0b5ee70bcc96ec3f111b5f
Michael Builov <mbuilov@gmail.com>
2017-09-11 01:03:28 +0400

    add EXE_AUX_TEMPLATEv, DLL_AUX_TEMPLATEv, UNIX_MOD_AUX_APPt, UNIX_MOD_AUX_APP

--- compilers/unixcc.mk
+++ compilers/unixcc.mk
@@ -28,5 +28,40 @@ $(call define_append,C_PREPARE_APP_VARS,$(newline)$$(C_PREPARE_UNIX_APP_VARS))
 # optimization
 $(call try_make_simple,C_PREPARE_APP_VARS,C_PREPARE_UNIX_APP_VARS)
 
+# auxiliary defines for EXE
+# $1 - $(call FORM_TRG,$t,$v)
+# $2 - $(call fixpath,$(MAP))
+# $t - EXE
+# $v - R,P
+# note: last line must be empty
+define EXE_AUX_TEMPLATEv
+$1:RPATH := $$(RPATH)
+$1:MAP := $2
+$1:$2
+
+endef
+
+# auxiliary defines for DLL
+# $1 - $(call FORM_TRG,$t,$v)
+# $2 - $(call fixpath,$(MAP))
+# $t - DLL
+# $v - R
+# note: last line must be empty
+define DLL_AUX_TEMPLATEv
+$1:MODVER := $(MODVER)
+$1:RPATH := $$(RPATH)
+$1:MAP := $2
+$1:$2
+
+endef
+
+# $1 - $(call fixpath,$(MAP))
+# $t - EXE or DLL
+UNIX_MOD_AUX_APPt = $(foreach v,$(call GET_VARIANTS,$t),$(call $t_AUX_TEMPLATEv,$(call FORM_TRG,$t,$v),$1))
+
+# auxiliary defines for EXE or DLL
+# define target-specific variables: RPATH, MAP, MODVER (only for DLL)
+UNIX_MOD_AUX_APP = $(foreach t,EXE DLL,$(if $($t),$(call UNIX_MOD_AUX_APPt,$(call fixpath,$(MAP)))))
+
 # protect variables from modifications in target makefiles
-$(call SET_GLOBAL,INST_RPATH C_PREPARE_UNIX_APP_VARS)
+$(call SET_GLOBAL,INST_RPATH C_PREPARE_UNIX_APP_VARS EXE_AUX_TEMPLATEv=t;v DLL_AUX_TEMPLATEv=t;v UNIX_MOD_AUX_APPt=t UNIX_MOD_AUX_APP)
