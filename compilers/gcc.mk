27f128684c14504d4318eca229c03feb02e0a81a
Michael Builov <mbuilov@gmail.com>
2017-10-29 23:32:27 +0400

    optimize

--- compilers/gcc.mk
+++ compilers/gcc.mk
@@ -131,8 +131,8 @@ DEF_DLL_LDFLAGS := -shared -Wl,--no-undefined $(CMN_LDFLAGS)
 # $4 - non-empty variant: R,P,D
 # target-specific: LIBS, DLLS, LIB_DIR
 CMN_LIBS = $(PIPE_OPTION) -o $1 $2 $(MK_RPATH_OPTION) $(MK_RPATH_LINK_OPTION) $(if $(firstword \
-  $(LIBS)$(DLLS)),-L$(LIB_DIR) $(addprefix -l,$(DLLS)) $(if $(LIBS),$(BSTATIC_OPTION) $(addprefix \
-  -l,$(addsuffix $(call DEP_SUFFIX,$3,$4,LIB),$(LIBS))) $(BDYNAMIC_OPTION)))
+  $(LIBS)$(DLLS)),-L$(LIB_DIR) $(addprefix -l,$(DLLS)) $(if $(LIBS),$(BSTATIC_OPTION) $(patsubst \
+  %,-l%$(call DEP_SUFFIX,$3,$4,LIB),$(LIBS)) $(BDYNAMIC_OPTION)))
 
 # specify what symbols to export from a dll/exe
 # target-specific: MAP
