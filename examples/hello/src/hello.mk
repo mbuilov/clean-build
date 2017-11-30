76656e879cd2b39d9bc93d8641e4c7795ad6af43
Michael Builov <mbuilov@gmail.com>
2017-11-30 15:19:04 +0300

    preliminary

--- examples/hello/src/hello.mk
+++ examples/hello/src/hello.mk
@@ -7,30 +7,32 @@
 # add rules for building C/C++ sources
 include $(dir $(lastword $(MAKEFILE_LIST)))../make/c.mk
 
-# add support for testing built executables
-include $(MTOP)/exts/ctest.mk
+# add support for testing built executables - define DO_TEST_EXE macro
+include $(MTOP)/extensions/ctest.mk
 
 # we will build S-variant of 'hello' executable - one with statically linked C runtime
 EXE := hello S
 SRC := hello.c
 
-# generate rules for testing built executable and creating 'hello.out' file
-$(DO_TEST_EXE)
+# generate rules for testing built executable and creating 'hello.out' output file
+OUT := $(DO_TEST_EXE)
+#$(call FORM_TRG,EXE,S).out
 
-# set makefile information for 'hello' - a phony target defined below
-# (this info is used by the SUP function, which pretty-prints what a rule is doing)
+# set makefile information for the 'hello' - a phony target defined below
+# (this information is used by the SUP function, which pretty-prints what a rule is doing)
+# Note: SET_MAKEFILE_INFO - one of predefined clean-build macros
 $(call SET_MAKEFILE_INFO,hello)
 
-# if executable output exist - print it to stderr
-# note: all output of rules should go to the stderr, stdout is used
-#  by the clean-build only for printing executed commands - this is
-#  needed for build-script generation
-# note: $| - automatic variable - list of order-only dependencies of the target
-# note: pass 1 as 4-th argument to SUP function to not update percents of executed makefiles
-hello: | $(call FORM_TRG,EXE,S).out
+# define custom rule - print output of tested executable to stderr
+# Note: any output of rules should go to stderr, stdout is used
+#  by clean-build only for printing executed commands - this is
+#  needed for the generation of build-script
+# Note: $| - automatic variable - list of order-only dependencies of the target
+# Note: pass 1 as 4-th argument to SUP function to not update percents of executed makefiles
+hello: | $(OUT)
 	$(call SUP,CAT,$|,,1)$(call CAT,$|) >&2
 
-# to complete predefined 'check' goal, it is needed to update target 'hello'
+# to complete predefined 'check' goal, it is needed to update our target 'hello'
 check: hello
 
 # specify that 'hello' - is not a file, it is a PHONY target
