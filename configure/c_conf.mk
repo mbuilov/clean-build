9607203674b4b6546e94265222e1148df1cb8330
Michael Builov <mbuilov@gmail.com>
2017-12-08 17:43:08 +0300

    excluded concept of preconfiguration

--- configure/c_conf.mk
+++
@@ -1,7 +0,0 @@
-#----------------------------------------------------------------------------------
-# clean-build - non-recursive build system based on GNU Make
-# Copyright (C) 2015-2017 Michael M. Builov, https://github.com/mbuilov/clean-build
-# Licensed under GPL version 2 or any later version, see COPYING
-#----------------------------------------------------------------------------------
-
-# support for C/C++ toolchain autoconfiguration
