8ddfe73ea6026537a6c1d2fa7d183669230ef0c7
Michael Builov <mbuilov@gmail.com>
2017-12-18 17:14:30 +0300

    more variables in lower case

--- stub/project.mk
+++ stub/project.mk
@@ -14,7 +14,7 @@
 # +- my_project/
 #   +- make/
 #   |  |- project.mk    (modified copy of this file)
-#   |  |- overrides.mk  (support for 'config' goal and processing of OVERRIDES variable)
+#   |  |- overrides.mk  (support for 'config' goal and processing of 'overrides' variable)
 #   |  |- submakes.mk   (support for sub-makefiles)
 #   |  |- c.mk          (support for building C/C++ sources)
 #   |  ...
@@ -25,7 +25,7 @@
 # where 'make' - directory of the project build system, some of the files in it can be the copies of the files from
 #  the clean-build 'stub' directory
 
-# the only case when variable 'top' is overridden - after completing project configuration
+# the only case when variable 'top' get overridden - after completing project configuration
 ifneq (override,$(origin top))
 
 # top - project root directory
@@ -39,24 +39,24 @@ override top := $(abspath $(dir $(lastword $(MAKEFILE_LIST)))..)
 #  deleted by the predefined 'distclean' goal
 cb_build := $(top)/build
 
-# version of the clean-build build system required by this project
-CLEAN_BUILD_REQUIRED_VERSION := 0.9.1
+# version of the clean-build build system required by the project
+clean_build_required_version := 0.9.1
 
 # next variables are needed for generating:
 # - header file with version info (see $(CBBS_ROOT)/extensions/version/version.mk)
 # - under Windows, resource files with version info (see $(CBBS_ROOT)/compilers/msvc/stdres.mk)
-PRODUCT_NAMES_H          := product_names.h
-PRODUCT_NAME             := Sample app
-PRODUCT_VENDOR_NAME      := Unkown Company/Author
-PRODUCT_VENDOR_COPYRIGHT := Copyright (C) 2018 Unkown Company/Author
+product_names_h          := product_names.h
+product_name             := Sample app
+product_vendor_name      := Unkown Company/Author
+product_vendor_copyright := Copyright (C) 2018 Unkown Company/Author
 
 # global product version
 # Note: this is the default value of 'modver' variable - per-module version number
 # format: major.minor.patch
-PRODUCT_VERSION := 1.0.0
+product_version := 1.0.0
 
-# Note: may pre-define clean-build macros here - predefined values will override default ones, e.g:
-#  PROJECT_SUPPORTED_TARGETS := DEVEL PRODUCTION
+# Note: may pre-define other clean-build macros here - predefined values will override default ones, e.g:
+#  project_supported_targets := DEVEL PRODUCTION
 
 # include core clean-build definitions, processing of the 'cb_config' and 'overrides' variables,
 #  check that the CBBS_ROOT variable - path to the clean-build - is defined
