de9db1835fa87688592efe9a9e55ce68c0dd3519
Michael Builov <mbuilov@gmail.com>
2018-01-25 13:34:38 +0300

    rename: gen_cmp_text -> cmp_text_rule

--- extensions/cmp.mk
+++ extensions/cmp.mk
@@ -4,6 +4,6 @@
 # Licensed under GPL version 2 or any later version, see COPYING
 #----------------------------------------------------------------------------------
 
-ifeq (,$(filter-out undefined environment,$(origin gen_cmp_text)))
+ifeq (,$(filter-out undefined environment,$(origin cmp_text_rule)))
 include $(dir $(lastword $(MAKEFILE_LIST)))impl/_cmp.mk
 endif
