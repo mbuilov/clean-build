2a0d5a52a7bbd45a104a99f8eedbe3583bfba41c
Michael Builov <mbuilov@gmail.com>
2018-01-29 18:03:40 +0300

    fix comments

--- install/impl/inst_text.mk
+++ install/impl/inst_text.mk
@@ -4,13 +4,12 @@
 # Licensed under GPL version 2 or any later version, see COPYING
 #----------------------------------------------------------------------------------
 
-# write lines of text $1 to file $2 by $3 lines at one time,
-#  then set access mode of written file $2 to $4
+# write lines of text $1 to the file $2 by $3 lines at one time, then set access mode of written file $2 to $4
 # note: pass non-empty 3-d argument to 'suppress' function to not colorize tool arguments
 # note: 'write_text' - defined in $(utils_mk) makefile (such as $(cb_dir)/utils/unix.mk)
 install_text = $(call suppress,GEN,$2,1)$(write_text)
 
-# set access mode of file $2 to $4
+# set access mode of the file $2 to $4
 # note: 'change_mode' defined in $(cb_dir)/utils/unix.mk, if it was included by $(cb_dir)/core/_defs.mk
 ifdef change_mode
 $(call define_append,install_text,$(newline)$$(call suppress,CHMOD,$$2,1)$$(call change_mode,$$4,$$2))
