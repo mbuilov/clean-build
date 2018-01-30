6501771f2d27281ab2705f90b390c6e25d5ab657
Michael Builov <mbuilov@gmail.com>
2018-01-30 15:56:44 +0300

    rework

--- compilers/suncc/cmn.mk
+++ compilers/suncc/cmn.mk
@@ -4,39 +4,44 @@
 # Licensed under GPL version 2 or any later version, see COPYING
 #----------------------------------------------------------------------------------
 
-# common suncc compiler definitions, included by $(CLEAN_BUILD_DIR)/compilers/suncc.mk
+# common suncc compiler definitions, included by $(cb_dir)/compilers/suncc.mk
 
-# $(SED) script to generate dependencies file from suncc compiler output
+# $(SED) script used to generate dependencies file from suncc compiler output
 #
-# note: '-xMD' cc option generates only partial makefile-dependency .d file
-#  - it doesn't include empty targets for dependency headers:
-#
+# note: cannot use '-xMD' cc option - it generates .d files without phony targets for the header files, e.g.:
 #  e.o: e.c
 #  e.o: e.h
-#  e.h:      <--- missing in generated .d file
+#  e.h:      <--- wasn't added to generated .d file
 #
 # $1 - compiler with options (unused)
 # $2 - target object file
 # $3 - prefixes of system includes to filter out, e.g. /usr/include/
-
-# /^$(tab)*\//!{p;d;}             - print all lines not started with optional tabs and /, start new circle
-# s/^\$(tab)*//;                  - strip-off leading tabs
-# $(foreach x,$3,\@^$x.*@d;)      - delete lines started with system include paths, start new circle
-# s@.*@&:\$(newline)$2: &@;w $2.d - make dependencies, then write to generated dep-file
-
-SUNCC_DEPS_SCRIPT = \
+#
+# /^$(tab)*\//!{p;d;}                         - print all lines not started with optional tabs and /, start new circle
+# s/^\$(tab)*//;                              - strip-off leading tabs
+# $(foreach x,$3,\@^$x.*@d;)                  - delete lines beginning with system include paths, start new circle
+# s@.*@&:\$(newline)$2: &@;w $(basename $2).d - construct dependencies, then write them to the generated dep-file
+#
+suncc_deps_script = \
 -e '/^$(tab)*\//!{p;d;}' \
 -e 's/^\$(tab)*//;$(foreach x,$3,\@^$x.*@d;)s@.*@&:\$(newline)$2: &@;w $(basename $2).d'
+
+# optimization: replace references to $(tab) and $(newline) with their values
+$(call expand_partially,suncc_deps_script,tab newline)
 
 # either just call compiler or call compiler and auto-generate dependencies
 # $1 - compiler with options
 # $2 - target object file
 # $3 - prefixes of system includes to filter out, e.g. /usr/include/
-ifdef NO_DEPS
-WRAP_SUNCC = $1
+# note: redirect compiler output to stderr, stdout is used for build-script generation (in verbose mode)
+# note: CBLD_NO_DEPS - defined in $(cb_dir)/core/_defs.mk
+# note: SED, GREP, NUL - defined in $(utils_mk) (e.g. $(cb_dir)/utils/unix.mk)
+ifeq (,$(CBLD_NO_DEPS))
+wrap_suncc = $1
 else
-WRAP_SUNCC = { { $1 -H 2>&1 && echo OK >&2; } | $(SED) -n $(SUNCC_DEPS_SCRIPT) 2>&1; } 3>&2 2>&1 1>&3 3>&- | grep OK > /dev/null
+wrap_suncc = { { $1 -H 2>&1 && $(ECHO) OK >&2; } | $(SED) -n $(suncc_deps_script) 2>&1; } 3>&2 2>&1 1>&3 3>&- | $(GREP) OK > $(NUL)
 endif
 
-# protect variables from modifications in target makefiles
-$(call SET_GLOBAL,SUNCC_DEPS_SCRIPT WRAP_SUNCC)
+# protect macros from modifications in target makefiles, allow tracing calls to them
+# note: trace namespace: suncc
+$(call set_global,suncc_deps_script wrap_suncc,suncc)
