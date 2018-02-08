ddc45d54382a7c677186d9c5cfa6e70d72340a66
Michael Builov <mbuilov@gmail.com>
2018-02-08 17:16:54 +0300

    GPL version 2 -> GPL version 3

--- core/_submakes.mk
+++ core/_submakes.mk
@@ -1,7 +1,7 @@
 #----------------------------------------------------------------------------------
 # clean-build - non-recursive build system based on GNU Make
-# Copyright (C) 2015-2017 Michael M. Builov, https://github.com/mbuilov/clean-build
-# Licensed under GPL version 2 or any later version, see COPYING
+# Copyright (C) 2015-2018 Michael M. Builov, https://github.com/mbuilov/clean-build
+# Licensed under GPL version 3 or any later version, see COPYING
 #----------------------------------------------------------------------------------
 
 # support for processing sub-makefiles
