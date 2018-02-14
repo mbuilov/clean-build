64469e0a60844e47838f6b4b2d25bdf821321f97
Michael Builov <mbuilov@gmail.com>
2018-02-14 11:57:24 +0300

    rename: CBLD_DONT_STRIP_PATH -> CBLD_DONT_STRIP_ENV_PATH, optimize copy_files/move_files, introduce print_short_string/write_string, rename: print_line -> print_short_line, print_text -> print_some_lines, write_text -> write_lines, fix bug in write_text

--- utils/cmd.mk
+++ utils/cmd.mk
@@ -13,8 +13,9 @@ MAKEFLAGS += -O
 
 ifneq (,$(filter /%,$(CURDIR)))
 $(error Unix version of Gnu Make is used with cmd.exe shell utilities - this configuration is not supported, \
-  please use Gnu Make executable built natively for the Windows platform (see https://github.com/mbuilov/gnumake-windows).$(newline)Tip: \
-  under Cygwin, native Gnu Make may be called as: /cygdrive/c/tools/gnumake-4.2.1.exe SED=C:/tools/sed.exe <args>)
+  please either select Unix shell utilities - start build with CBLD_UTILS=unix or use Gnu Make executable built \
+  natively for the Windows platform (see https://github.com/mbuilov/gnumake-windows).$(newline)Tip: \
+  under Cygwin, native Windows build may be started like: /cygdrive/c/tools/gnumake-4.2.1.exe SED=C:/tools/sed.exe <goals>)
 endif
 
 # shell must be cmd.exe, not /bin/sh (if build is started from the Cygwin/Msys shell)
@@ -34,9 +35,9 @@ endif
 endif # SHELL != cmd.exe
 
 # by default, strip off paths to Unix tools from the PATH - to use only native windows shell utilities (find, del, move, md, etc.)
-CBLD_DONT_STRIP_PATH ?=
+CBLD_DONT_STRIP_ENV_PATH ?=
 
-ifndef CBLD_DONT_STRIP_PATH
+ifeq (,$(CBLD_DONT_STRIP_ENV_PATH))
 
 # note: Cygwin paths may look like: C:\cygwin64\usr\local\bin;C:\cygwin64\bin
 # note: Msys paths may look like: C:\msys64\mingw64\bin;C:\msys64\usr\local\bin;C:\msys64\usr\bin
@@ -58,7 +59,7 @@ endif
 # note: pass 1 as third argument to 'config_remember_vars' - to save a new value of the PATH
 $(call config_remember_vars,PATH,1,1)
 
-endif # !CBLD_DONT_STRIP_PATH
+endif # !CBLD_DONT_STRIP_ENV_PATH
 
 # script to print prepared environment in verbose mode (used for generating one-big-build instructions batch file)
 # note: 'print_env' - used by $(cb_dir)/core/all.mk
@@ -137,11 +138,7 @@ del_files_or_dirs  = $(call xcmd,del_files_or_dirs1,$(ospath),$(CBLD_MAX_PATH_AR
 
 # filter the output of the 'copy' or 'move' command:
 #  - if there is anything other than the filtered-out string "        1", result will be zero - assume copy/move failed
-ifdef FIND
 filter_copy_output := $(FIND) /V"        1"
-else
-filter_copy_output := $(FINDSTR) /VC:"        1"
-endif
 
 # code for suppressing output of the 'copy' command, like
 # "        1 file(s) copied."
@@ -160,7 +157,7 @@ suppress_move_output := $(suppress_copy_output)
 # note: $6 - empty on first call, $(newline) on next calls
 # note: if path to the directory/file $2 contains a space, it must be in double-quotes: "1 2\3 4"
 copy_files1 = $(if $6,$(quiet))for %%f in ($1) do $(COPY) /y /b %%f $2$(suppress_copy_output)
-copy_files  = $(if $(word 2,$1),$(call xcmd,copy_files1,$(ospath),$(CBLD_MAX_PATH_ARGS),$(call \
+copy_files  = $(if $(findstring $(space),$1),$(call xcmd,copy_files1,$(ospath),$(CBLD_MAX_PATH_ARGS),$(call \
   ospath,$2),,,),$(COPY) /y /b $(call ospath,$1 $2)$(suppress_copy_output))
 
 # move file(s) (long list) preserving modification date:
@@ -169,7 +166,7 @@ copy_files  = $(if $(word 2,$1),$(call xcmd,copy_files1,$(ospath),$(CBLD_MAX_PAT
 # note: $6 - empty on first call, $(newline) on next calls
 # note: if path to the directory/file $2 contains a space, it must be in double-quotes: "1 2\3 4"
 move_files1 = $(if $6,$(quiet))for %%f in ($1) do $(MOVE) /y %%f $2$(suppress_move_output)
-move_files  = $(if $(word 2,$1),$(call xcmd,move_files1,$(ospath),$(CBLD_MAX_PATH_ARGS),$(call \
+move_files  = $(if $(findstring $(space),$1),$(call xcmd,move_files1,$(ospath),$(CBLD_MAX_PATH_ARGS),$(call \
   ospath,$2),,,),$(MOVE) /y $(call ospath,$1 $2)$(suppress_move_output))
 
 # update modification date of given file(s) or create file(s) if they do not exist
@@ -197,50 +194,78 @@ create_dir = $(MKDIR) $(ospath)
 # note: if path to a file contains a space, it must be in double-quotes: "1 2\3 4"
 compare_files = $(FC) /t $(call ospath,$1 $2)
 
+# print content of a given file (to stdout, for redirecting it to output file)
+# note: if path to the file $1 contains a space, it must be in double-quotes: "1 2\3 4"
+cat_file = $(TYPE) $(ospath)
+
 # escape program argument to pass it via shell: 1 " 2 -> "1 "" 2"
 shell_escape = "$(subst ","",$(subst \",\\",$(subst %,%%,$1)))"
-
-# escape special characters in unquoted argument of 'echo' or 'set' builtin command of cmd.exe
-unquoted_escape = $(subst $(open_brace),^$(open_brace),$(subst $(close_brace),^$(close_brace),$(subst \
-  %,%%,$(subst <,^<,$(subst >,^>,$(subst |,^|,$(subst &,^&,$(subst ",^",$(subst ^,^^,$1)))))))))
 
 # escape command line argument to pass it to $(SED)
 # note: assume GNU sed is used, which understands \n and \t escape sequences
 sed_expr = $(shell_escape)
 
-# print content of a given file (to stdout, for redirecting it to output file)
-# note: if path to the file $1 contains a space, it must be in double-quotes: "1 2\3 4"
-cat_file = $(TYPE) $(ospath)
+# escape special characters in unquoted argument of 'echo' or 'set' builtin commands of cmd.exe
+unquoted_escape = $(subst $(open_brace),^$(open_brace),$(subst $(close_brace),^$(close_brace),$(subst \
+  %,%%,$(subst <,^<,$(subst >,^>,$(subst |,^|,$(subst &,^&,$(subst ",^",$(subst ^,^^,$1)))))))))
 
-# print one line of text (to stdout, for redirecting it to output file)
+# print short string (to stdout, for redirecting it to output file)
+# note: there must be no $(newline)s in the string $1
+# note: surround whole expression by braces to specify the end of argument
+# NOTE: printed string length must not exceed the maximum command line length (8191 characters)
+print_short_string = (set/p=$(unquoted_escape))<NUL
+
+# write batch of text tokens to output file or to stdout (for redirecting it to output file)
+# $1 - tokens list, where entries are processed by $(call sptokens,$(unquoted_escape))
+# $2 - if not empty, then a file to print to (path to the file may contain spaces)
+# $3 - text to prepend before the command when $6 is non-empty
+# $4 - text to prepend before the command when $6 is empty
+# $6 - empty if overwrite file $2, non-empty if append text to it
+# note: there must be no $(newline)s among text tokens
+# note: if path to the file $2 contains a space, it must be in double-quotes: "1 2\3 4"
+# NOTE: printed batch length must not exceed the maximum command line length (8191 characters)
+# note: used by 'write_string' macro
+write_string1 = $(if $6,$3,$4)(set/p=$(tospaces))<NUL$(if $2,>$(if $6,>) $2)
+
+# write string $1 to the file $2 by $3 tokens at one time
+# note: there must be no $(newline)s in the string $1
+# note: if path to the file $2 contains a space, it must be in double-quotes: "1 2\3 4"
+# NOTE: number $3 must be adjusted so printed at one time text length will not exceed the maximum command length (8191 characters)
+# NOTE: nothing is printed if string $1 is empty
+write_string = $(call xargs,write_string1,$(call sptokens,$(unquoted_escape)),$3,$2,$(quiet),,,$(newline))
+
+# print one short line of text (to stdout, for redirecting it to output file)
 # note: line must not contain $(newline)s
-# note: line will be ended with CRLF
-# note: use dot '.' after 'echo' to print just an empty line if $(unquoted_escape) expands to nothing
-# note: surround whole expression by braces to avoid adding extra space at end of line in this example: $(call print_line,text) > f.out
+# note: line will be ended with CRLF: line -> line\n
+# note: use dot '.' after 'echo' to print just an empty line if text $1 is empty
+# note: surround whole expression by braces to specify the end of argument
 # NOTE: printed line length must not exceed the maximum command line length (8191 characters)
-print_line = (echo.$(unquoted_escape))
+print_short_line = (echo.$(unquoted_escape))
 
-# print lines of text to output file or to stdout (for redirecting it to output file)
-# $1 - non-empty lines list, where entries are processed by $(unescape)
+# print small batch of short text lines (to stdout, for redirecting it to output file)
+# note: each line will be ended with CRLF: line1$(newline)line2 -> line1\nline2\n
+# NOTE: total text length must not exceed the maximum command line length (8191 characters)
+print_some_lines = (echo.$(subst $(newline),$(close_brace)&&$(open_brace)echo.,$(unquoted_escape)))
+
+# write batch of text lines to output file or to stdout (for redirecting it to output file)
+# $1 - lines list, where entries are processed by $(call unspaces,$(unquoted_escape))
 # $2 - if not empty, then a file to print to (path to the file may contain spaces)
 # $3 - text to prepend before the command when $6 is non-empty
 # $4 - text to prepend before the command when $6 is empty
 # $6 - empty if overwrite file $2, non-empty if append text to it
 # note: if path to the file $2 contains a space, it must be in double-quotes: "1 2\3 4"
-# NOTE: total text length must not exceed the maximum command line length (8191 characters)
-print_lines = $(if $6,$3,$4)$(call tospaces,(echo.$(subst \
-  $(space),$(close_brace)&&$(open_brace)echo.,$(unquoted_escape))))$(if $2,>$(if $6,>) $2)
-
-# print lines of text (to stdout, for redirecting it to output file)
-# note: each line will be ended with CRLF
-# NOTE: total text length must not exceed the maximum command line length (8191 characters)
-print_text = $(if $(findstring $(newline),$1),$(call print_lines,$(subst $(newline),$$(empty) $$(empty),$(unspaces))),$(print_line))
+# note: each line will be ended with CRLF: line1$(space)line2 -> line1\nline2\n
+# NOTE: printed batch length must not exceed the maximum command line length (8191 characters)
+# note: used by 'write_lines' macro
+write_lines1 = $(if $6,$3,$4)(echo.$(call tospaces,$(subst $(space),$(close_brace)&&$(open_brace)echo.,$1)))$(if $2,>$(if $6,>) $2)
 
 # write lines of text $1 to the file $2 by $3 lines at one time
 # note: if path to the file $2 contains a space, it must be in double-quotes: "1 2\3 4"
 # NOTE: any line must be less than the maximum command length (8191 characters)
 # NOTE: number $3 must be adjusted so printed at one time text length will not exceed the maximum command length (8191 characters)
-write_text = $(call xargs,print_lines,$(subst $(newline),$$(empty) $$(empty),$(unspaces)),$3,$2,$(quiet),,,$(newline))
+# NOTE: nothing is printed if text $1 is empty
+write_lines = $(call xargs,write_lines1,$(subst \
+  $(newline),$$(empty) $$(empty),$(call unspaces,$(unquoted_escape))),$3,$2,$(quiet),,,$(newline))
 
 # create symbolic link $2 -> $1
 # note: UNIX-specific, so not defined for WINDOWS
@@ -289,22 +314,29 @@ show_tool_vars_end = $(newline)@echo endlocal
 # filter command's output through pipe, then send it to stderr
 # $1 - command
 # $2 - filtering expression to filter stdout, must be non-empty, for example: |$(FINDSTR) /BC:ABC |$(FINDSTR) /BC:CDE
-filter_output = (($1 2>&1 && echo ok>&2)$2)3>&2 2>&1 1>&3|$(FINDSTR) /BC:ok>$(NUL)
+# note: when sending output of echo to stderr, cmd.exe inserts a space before '>&2', e.g.:
+#  3>&2 2>&1 1>&3 echo a >&2     |c:\cygwin64\bin\xxd.exe   -> 00000000: 6120 0d0a
+#  3>&2 2>&1 1>&3 echo a>&2      |c:\cygwin64\bin\xxd.exe   -> 00000000: 6120 0d0a
+#  3>&2 2>&1 1>&3 (echo a)>&2    |c:\cygwin64\bin\xxd.exe   -> 00000000: 6120 0d0a
+#  3>&2 2>&1 1>&3 ^(echo a^)>&2  |c:\cygwin64\bin\xxd.exe   -> invalid command
+#  (3>&2 2>&1 1>&3 ^(echo a^)>&2)|c:\cygwin64\bin\xxd.exe   -> 00000000: 610d 0a
+filter_output = (($1 2>&1 &&^(echo ok^)>&2)$2)3>&2 2>&1 1>&3|$(FINDSTR) /XC:ok >$(NUL)
 
 # remember value of variables that may be taken from the environment
-$(call config_remember_vars,SHELL CBLD_DONT_STRIP_PATH CBLD_MAX_PATH_ARGS NUL DEL RD CD FIND FINDSTR COPY MOVE MKDIR FC SED TYPE)
+$(call config_remember_vars,SHELL CBLD_DONT_STRIP_ENV_PATH CBLD_MAX_PATH_ARGS NUL DEL RD CD FIND FINDSTR COPY MOVE MKDIR FC SED TYPE)
 
 # protect macros from modifications in target makefiles,
 # do not trace calls to macros used in ifdefs, exported to the environment of called tools or modified via operator +=
-$(call set_global,SHELL CBLD_DONT_STRIP_PATH CBLD_MAX_PATH_ARGS NUL DEL RD CD FIND FINDSTR COPY MOVE MKDIR FC SED TYPE \
+$(call set_global,SHELL CBLD_DONT_STRIP_ENV_PATH CBLD_MAX_PATH_ARGS NUL DEL RD CD FIND FINDSTR COPY MOVE MKDIR FC SED TYPE \
   create_simlink change_mode CBLD_ENABLE_ANSI_COLORS)
 
 # protect macros from modifications in target makefiles, allow tracing calls to them
 # note: trace namespace: utils
 $(call set_global,print_env=project_exported_vars delete_files delete_dirs try_delete_dirs delete_files_in1 delete_files_in \
   del_files_or_dirs1 del_files_or_dirs filter_copy_output suppress_copy_output suppress_move_output copy_files1 copy_files \
-  move_files1 move_files touch_files1 touch_files create_dir compare_files shell_escape unquoted_escape sed_expr cat_file \
-  print_line print_lines print_text write_text execute_in del_on_fail install_dir install_files filter_output,utils)
+  move_files1 move_files touch_files1 touch_files create_dir compare_files cat_file shell_escape sed_expr unquoted_escape \
+  print_short_string write_string1 write_string print_short_line print_some_lines write_lines1 write_lines \
+  execute_in del_on_fail install_dir install_files filter_output,utils)
 
 # protect macros from modifications in target makefiles, allow tracing calls to them
 # note: trace namespace: core
