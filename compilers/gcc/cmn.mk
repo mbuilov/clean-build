bb7b8b471135ac8125eaaf73c761bf1771db14c9
Michael Builov <mbuilov@gmail.com>
2018-02-19 17:28:47 +0300

    introduce 'gcc_path' macro - for converting paths passed to gcc tools

--- compilers/gcc/cmn.mk
+++ compilers/gcc/cmn.mk
@@ -16,14 +16,25 @@ pipe_option := -pipe
 # note: CBLD_NO_DEPS - defined in $(cb_dir)/core/_defs.mk
 auto_deps_flags := $(if $(CBLD_NO_DEPS),,-MMD -MP)
 
-# write by that number of tokens at a time while generating a response file
-# note: with too many tokens it is possible to exceed maximum command string length (possible of 4096 characters only)
-# note: CBLD_MAX_PATH_ARGS is defined in $(utils_mk) - e.g. $(cb_dir)/utils/unix.mk
-CBLD_LINK_RESP_WRITE_BY ?= $(CBLD_MAX_PATH_ARGS)
+# assume native gcc is used, e.g. Windows version of gcc under Windows/Msys, except under Cygwin
+CBLD_IS_NATIVE_GCC ?= $(filter-out CYGWIN%,$(CBLD_OS))
+
+# convert path from Gnu Make representation to the form accepted by GCC tools (e.g. to pass path to headers)
+ifneq (,$(CBLD_IS_NATIVE_GCC:0=))
+gcc_path = $(ospath)
+else
+gcc_path = $1
+endif
+
+# make compiler options string to specify search path of included headers
+# note: assume there are no spaces in include paths
+# note: overwrite 'mk_include_option' macro of $(cb_dir)/types/c/c_base.mk
+mk_include_option = $(addprefix -I,$(gcc_path))
 
 # It is possible to exceed maximum command string length if linking too many objects at once (especially on Windows), to avoid
 #  command line length limitation, it may be required to use a response file - @<file>
 # Number of linker arguments so that a response file is required for the linking
+# Also, this is a number of tokens (words) written at once while generating a response file
 # note: CBLD_MAX_PATH_ARGS is defined in $(utils_mk) - e.g. $(cb_dir)/utils/unix.mk
 CBLD_LINK_ARGS_LIMIT ?= $(CBLD_MAX_PATH_ARGS)
 
@@ -32,8 +43,8 @@ CBLD_LINK_ARGS_LIMIT ?= $(CBLD_MAX_PATH_ARGS)
 # $3 - linker command (linker executable with flags)
 # $4 - additional linker arguments (object files, libraries, flags)
 # $5 - name of .rsp file, if empty, then .rsp file is not needed
-gcc_rsp_wrap1 = $(if $5,$(call suppress,GEN,$5)$(call write_string,$4,$5,$(CBLD_LINK_RESP_WRITE_BY))$(newline))$(call \
-  suppress,$1,$2)$3 $(if $5,@$5,$4)
+gcc_rsp_wrap1 = $(if $5,$(call suppress,GEN,$5)$(call write_string,$4,$5,$(CBLD_LINK_ARGS_LIMIT))$(newline))$(call \
+  suppress,$1,$2)$3 $(if $5,@$(call gcc_path,$5),$4)
 
 # check if linker command line is too long - it is needed to create a response file for passing arguments to the linker
 # $1 - linked target type: $(tm)EXE, $(tm)DLL, ...
@@ -44,12 +55,16 @@ gcc_rsp_wrap1 = $(if $5,$(call suppress,GEN,$5)$(call write_string,$4,$5,$(CBLD_
 gcc_rsp_wrap = $(call gcc_rsp_wrap1,$1,$2,$3,$4,$(if $(word $(CBLD_LINK_ARGS_LIMIT),$4),$(objdir)/link.rsp))
 
 # remember values the variables possibly defined in the environment
-$(call config_remember_vars,CBLD_LINK_RESP_WRITE_BY CBLD_LINK_ARGS_LIMIT)
+$(call config_remember_vars,CBLD_IS_NATIVE_GCC CBLD_LINK_ARGS_LIMIT)
 
 # protect variables from modifications in target makefiles
 # do not trace calls to macros used in ifdefs, exported to the environment of called tools or modified via operator +=
-$(call set_global,CBLD_LINK_RESP_WRITE_BY CBLD_LINK_ARGS_LIMIT)
+$(call set_global,CBLD_IS_NATIVE_GCC CBLD_LINK_ARGS_LIMIT)
 
 # protect variables from modifications in target makefiles
 # note: trace namespace: gcc
-$(call set_global,wlprefix pipe_option auto_deps_flags gcc_rsp_wrap1 gcc_rsp_wrap,gcc)
+$(call set_global,wlprefix pipe_option auto_deps_flags gcc_path gcc_rsp_wrap1 gcc_rsp_wrap,gcc)
+
+# protect variables from modifications in target makefiles
+# note: trace namespace: c_base
+$(call set_global,mk_include_option,c_base)
