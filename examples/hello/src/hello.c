b560e176c3bd25ad21aad13c23f276f11d1cc15c
Michael Builov <mbuilov@gmail.com>
2018-02-23 20:26:07 +0400

    fix copyright header

--- examples/hello/src/hello.c
+++ examples/hello/src/hello.c
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------------
- * clean-build - non-recursive cross-platform build system based on GNU Make
+ * clean-build - non-recursive cross-platform build system based on GNU Make v3.81
  * Copyright (C) 2015-2018 Michael M. Builov, https://github.com/mbuilov/clean-build
  * Licensed under GPL version 3 or any later version, see COPYING
  *--------------------------------------------------------------------------------*/
--- extensions/moc.mk
+++ extensions/moc.mk
@@ -1,5 +1,5 @@
 #----------------------------------------------------------------------------------
-# clean-build - non-recursive cross-platform build system based on GNU Make
+# clean-build - non-recursive cross-platform build system based on GNU Make v3.81
 # Copyright (C) 2015-2017 Michael M. Builov, https://github.com/mbuilov/clean-build
 # Licensed under GPL version 2 or any later version, see COPYING
 #----------------------------------------------------------------------------------
