b22b59fda5ee35c60765e0f604e591aaf93a13f9
Michael Builov <mbuilov@gmail.com>
2018-02-26 12:38:38 +0300

    add more tests

--- tests/function_tests.mk
+++ tests/function_tests.mk
@@ -174,37 +174,37 @@ endif
 #######################################
 # test 'isless'
 
-ifneq (,$(call is_less,0,0))
+ifneq ("","$(call is_less,0,0)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less,1,0))
+ifneq ("","$(call is_less,1,0)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less,0,1))
+ifeq ("","$(call is_less,0,1)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less,1,1))
+ifneq ("","$(call is_less,1,1)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less,2,1))
+ifneq ("","$(call is_less,2,1)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less,1,2))
+ifeq ("","$(call is_less,1,2)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less,100,100))
+ifneq ("","$(call is_less,100,100)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less,100,1))
+ifneq ("","$(call is_less,100,1)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less,1,100))
+ifeq ("","$(call is_less,1,100)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less,200,100))
+ifneq ("","$(call is_less,200,100)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less,100,200))
+ifeq ("","$(call is_less,100,200)")
 $(error test failed!)
 endif
 
@@ -219,113 +219,113 @@ endif
 # test 'is_less_float'
 
 # 1:  0    <->  0
-ifneq (,$(call is_less_float,0,0))
+ifneq ("","$(call is_less_float,0,0)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less_float,1,0))
+ifneq ("","$(call is_less_float,1,0)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less_float,0,1))
+ifeq ("","$(call is_less_float,0,1)")
 $(error test failed!)
 endif
 
 # 2:  0.   <->  0.
-ifneq (,$(call is_less_float,0.,0.))
+ifneq ("","$(call is_less_float,0.,0.)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less_float,1.,0.))
+ifneq ("","$(call is_less_float,1.,0.)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less_float,0.,1.))
+ifeq ("","$(call is_less_float,0.,1.)")
 $(error test failed!)
 endif
 
 # 3:  0.   <->  0
-ifneq (,$(call is_less_float,0.,0))
+ifneq ("","$(call is_less_float,0.,0)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less_float,1.,0))
+ifneq ("","$(call is_less_float,1.,0)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less_float,0.,1))
+ifeq ("","$(call is_less_float,0.,1)")
 $(error test failed!)
 endif
 
 # 4:  0    <->  0.
-ifneq (,$(call is_less_float,0,0.))
+ifneq ("","$(call is_less_float,0,0.)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less_float,1,0.))
+ifneq ("","$(call is_less_float,1,0.)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less_float,0,1.))
+ifeq ("","$(call is_less_float,0,1.)")
 $(error test failed!)
 endif
 
 # 5:  0.0  <->  0.0
-ifneq (,$(call is_less_float,0.0,0.0))
+ifneq ("","$(call is_less_float,0.0,0.0)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less_float,1.0,0.0))
+ifneq ("","$(call is_less_float,1.0,0.0)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less_float,0.0,1.0))
+ifeq ("","$(call is_less_float,0.0,1.0)")
 $(error test failed!)
 endif
 
 # 6:  0    <->  0.0
-ifneq (,$(call is_less_float,0,0.0))
+ifneq ("","$(call is_less_float,0,0.0)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less_float,1,0.0))
+ifneq ("","$(call is_less_float,1,0.0)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less_float,0,1.0))
+ifeq ("","$(call is_less_float,0,1.0)")
 $(error test failed!)
 endif
 
 # 7:  0.0  <->  0
-ifneq (,$(call is_less_float,0.0,0))
+ifneq ("","$(call is_less_float,0.0,0)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less_float,1.0,0))
+ifneq ("","$(call is_less_float,1.0,0)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less_float,0.0,1))
+ifeq ("","$(call is_less_float,0.0,1)")
 $(error test failed!)
 endif
 
 # 8:  0.   <->  0.0
-ifneq (,$(call is_less_float,0.,0.0))
+ifneq ("","$(call is_less_float,0.,0.0)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less_float,1.,0.0))
+ifneq ("","$(call is_less_float,1.,0.0)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less_float,0.,1.0))
+ifeq ("","$(call is_less_float,0.,1.0)")
 $(error test failed!)
 endif
 
 # 9:  0.0  <->  0.
-ifneq (,$(call is_less_float,0.0,0.))
+ifneq ("","$(call is_less_float,0.0,0.)")
 $(error test failed!)
 endif
-ifneq (,$(call is_less_float,1.0,0.))
+ifneq ("","$(call is_less_float,1.0,0.)")
 $(error test failed!)
 endif
-ifeq (,$(call is_less_float,0.0,1.))
-$(error test failed!)
-endif
-
-ifeq (,$(call is_less_float,0.33333,2))
+ifeq ("","$(call is_less_float,0.0,1.)")
 $(error test failed!)
 endif
 
-ifeq (,$(call is_less_float,0.33333,0.4))
+ifeq ("","$(call is_less_float,0.33333,2)")
 $(error test failed!)
 endif
 
-ifneq (,$(call is_less_float,0.33333,0.2222222))
+ifeq ("","$(call is_less_float,0.33333,0.4)")
+$(error test failed!)
+endif
+
+ifneq ("","$(call is_less_float,0.33333,0.2222222)")
 $(error test failed!)
 endif
 
@@ -382,32 +382,32 @@ $(error test failed!)
 endif
 
 #######################################
-# test 'trim'
+# test 'uniq'
 
-ifneq ("a b c d e f g h i j k l m n o","$(call trim,a b c d e f g h i j k l m n o p)")
+ifneq ("1 2 3","$(call uniq,1 2 3 2 1)")
 $(error test failed!)
 endif
 
 #######################################
 # test 'neq'
 
-ifneq (,$(call neq,1 2,1 2))
+ifneq ("","$(call neq,1 2,1 2)")
 $(error test failed!)
 endif
 
-ifeq (,$(call neq,1 2,2 3))
+ifeq ("","$(call neq,1 2,2 3)")
 $(error test failed!)
 endif
 
-ifeq (,$(call neq,1 2,1 2 ))
+ifeq ("","$(call neq,1 2,1 2 )")
 $(error test failed!)
 endif
 
-ifeq (,$(call neq,1 2, 1 2))
+ifeq ("","$(call neq,1 2, 1 2)")
 $(error test failed!)
 endif
 
-ifeq (,$(call neq,1 2,1  2))
+ifeq ("","$(call neq,1 2,1  2)")
 $(error test failed!)
 endif
 
@@ -445,13 +445,20 @@ $(error test failed!)
 endif
 
 #######################################
+# test 'trim'
+
+ifneq ("a b c d e f g h i j k l m n o","$(call trim,a b c d e f g h i j k l m n o p)")
+$(error test failed!)
+endif
+
+#######################################
 # test 'normp'
 
 ifneq ("1/3/7","$(call normp,1/3//5/.././6/../7)")
 $(error test failed!)
 endif
 
-ifneq ("/1/3","$(call normp,/./1/3//5/.././6/6/../../7/..)")
+ifneq ("/1/3","$(call normp,/./1/2/../3//5/.././6/6/../../7/..)")
 $(error test failed!)
 endif
 
@@ -459,6 +466,10 @@ endif
 # test 'cmn_path'
 
 ifneq ("aa/","$(call cmn_path,aa/bb/cc/,aa/dd/qq/)")
+$(error test failed!)
+endif
+
+ifneq ("a/a/a/a/","$(call cmn_path,a/a/a/a/bb/cc/,a/a/a/a/dd/qq/)")
 $(error test failed!)
 endif
 
@@ -481,62 +492,349 @@ endif
 #######################################
 # test 'back_prefix'
 
+ifneq ("../../../","$(call back_prefix,a/b/c/)")
+$(error test failed!)
+endif
+
+ifneq ("../../../","$(call back_prefix,a x/b y/c/)")
+$(error test failed!)
+endif
+
 #######################################
 # test 'relpath'
+
+ifneq ("../../dd/qq/","$(call relpath,/aa/bb/cc/,/aa/dd/qq/)")
+$(error test failed!)
+endif
 
 #######################################
 # test 'ver_major'
 
+ifneq ("0","$(call ver_major,)")
+$(error test failed!)
+endif
+ifneq ("1","$(call ver_major,1)")
+$(error test failed!)
+endif
+ifneq ("1","$(call ver_major,1.2)")
+$(error test failed!)
+endif
+ifneq ("1","$(call ver_major,1.2.3)")
+$(error test failed!)
+endif
+
 #######################################
 # test 'ver_minor'
+
+ifneq ("0","$(call ver_minor,)")
+$(error test failed!)
+endif
+ifneq ("0","$(call ver_minor,1)")
+$(error test failed!)
+endif
+ifneq ("2","$(call ver_minor,1.2)")
+$(error test failed!)
+endif
+ifneq ("2","$(call ver_minor,1.2.3)")
+$(error test failed!)
+endif
 
 #######################################
 # test 'ver_patch'
 
+ifneq ("0","$(call ver_patch,)")
+$(error test failed!)
+endif
+ifneq ("0","$(call ver_patch,1)")
+$(error test failed!)
+endif
+ifneq ("0","$(call ver_patch,1.2)")
+$(error test failed!)
+endif
+ifneq ("3","$(call ver_patch,1.2.3)")
+$(error test failed!)
+endif
+
 #######################################
 # test 'ver_compatible'
+
+ifeq ("","$(call ver_compatible,1.2.3,1)")
+$(error test failed!)
+endif
+
+ifneq ("","$(call ver_compatible,1,1.2.3)")
+$(error test failed!)
+endif
+
+ifeq ("","$(call ver_compatible,1.2.3,1.1)")
+$(error test failed!)
+endif
+
+ifneq ("","$(call ver_compatible,1.1,1.2.3)")
+$(error test failed!)
+endif
+
+ifeq ("","$(call ver_compatible,1.2.3,1.2)")
+$(error test failed!)
+endif
+
+ifneq ("","$(call ver_compatible,1.2,1.2.3)")
+$(error test failed!)
+endif
+
+ifeq ("","$(call ver_compatible,1.2.3,1.2.3)")
+$(error test failed!)
+endif
+
+ifneq ("","$(call ver_compatible,2,1.2.3)")
+$(error test failed!)
+endif
 
 #######################################
 # test 'get_dir'
 
+ifneq ("","$(call get_dir,1)")
+$(error test failed!)
+endif
+
+ifneq ("/1","$(call get_dir,/1/2)")
+$(error test failed!)
+endif
+
+ifneq ("1/2","$(call get_dir,1/2/)")
+$(error test failed!)
+endif
+
+ifneq ("/1/2","$(call get_dir,/1/2/)")
+$(error test failed!)
+endif
+
 #######################################
 # test 'split_dirs'
+
+ifneq ("1 1/2 1/2/3","$(call split_dirs,1/2/3)")
+$(error test failed!)
+endif
+
+ifneq ("/1 /1/2 /1/2/3","$(call split_dirs,/1/2/3)")
+$(error test failed!)
+endif
+
+ifneq ("/1 /1/2","$(call split_dirs,/1/2)")
+$(error test failed!)
+endif
+
+ifneq ("1 1/2","$(call split_dirs,1/2)")
+$(error test failed!)
+endif
+
+ifneq ("1","$(call split_dirs,1)")
+$(error test failed!)
+endif
 
 #######################################
 # test 'mk_dir_deps'
 
+define H
+1/2:| 1
+1/2/3:| 1/2
+endef
+
+ifneq ("$(value H)","$(call mk_dir_deps,1 1/2 1/2/3)")
+$(error test failed!)
+endif
+
+define H
+xx/1/2:| xx/1
+xx/1/2/3:| xx/1/2
+endef
+
+ifneq ("$(value H)","$(call mk_dir_deps,1 1/2 1/2/3,xx/)")
+$(error test failed!)
+endif
+
+define H
+1/2:| 1
+endef
+
+ifneq ("$(value H)","$(call mk_dir_deps,1 1/2)")
+$(error test failed!)
+endif
+
+define H
+/xx/1/2:| /xx/1
+endef
+
+ifneq ("$(value H)","$(call mk_dir_deps,1 1/2,/xx/)")
+$(error test failed!)
+endif
+
+ifneq ("","$(call mk_dir_deps,1,xx/)")
+$(error test failed!)
+endif
+
 #######################################
 # test 'lazy_simple'
+
+A = $(call lazy_simple,A,$B+$C)
+
+B := 1
+C := 2
+
+ifneq ("1+2","$A")
+$(error test failed!)
+endif
+
+ifneq ("simple","$(flavor A)")
+$(error test failed!)
+endif
 
 #######################################
 # test 'define_append'
 
+A = 1
+
+ifneq ("","$(call define_append,A,$$B)")
+$(error test failed!)
+endif
+
+ifneq ("1$$B","$(value A)")
+$(error test failed!)
+endif
+
+ifneq ("recursive","$(flavor A)")
+$(error test failed!)
+endif
+
 #######################################
 # test 'define_prepend'
+
+A = 1
+
+ifneq ("","$(call define_prepend,A,$$B)")
+$(error test failed!)
+endif
+
+ifneq ("$$B1","$(value A)")
+$(error test failed!)
+endif
+
+ifneq ("recursive","$(flavor A)")
+$(error test failed!)
+endif
 
 #######################################
 # test 'append_simple'
 
+A := 1
+
+ifneq ("","$(call append_simple,A,2)")
+$(error test failed!)
+endif
+
+ifneq ("12","$A")
+$(error test failed!)
+endif
+
+ifneq ("simple","$(flavor A)")
+$(error test failed!)
+endif
+
 #######################################
 # test 'prepend_simple'
+
+A := 1
+
+ifneq ("","$(call prepend_simple,A,2)")
+$(error test failed!)
+endif
+
+ifneq ("21","$A")
+$(error test failed!)
+endif
+
+ifneq ("simple","$(flavor A)")
+$(error test failed!)
+endif
 
 #######################################
 # test 'subst_var_refs'
 
+H = 1$(D)+$(BB)+4
+D = 2
+BB = a b
+
+ifneq ("12+a b+4","$(call subst_var_refs,$(value H),D BB)")
+$(error test failed!)
+endif
+
 #######################################
 # test 'expand_partially'
+
+H = 1$(D)+$(BB)+4
+D = 2
+BB = a b
+
+ifneq ("","$(call expand_partially,H,D BB)")
+$(error test failed!)
+endif
+
+ifneq ("12+a b+4","$(value H)")
+$(error test failed!)
+endif
+
+ifneq ("recursive","$(flavor H)")
+$(error test failed!)
+endif
 
 #######################################
 # test 'remove_var_refs'
 
+H = 1$(D)+$(BB)+4
+
+ifneq ("1++4","$(call remove_var_refs,$(value H),D BB)")
+$(error test failed!)
+endif
+
 #######################################
 # test 'try_make_simple'
+
+H = 1$(D)+$(BB)+4
+D := 2
+BB := a b
+
+ifneq ("","$(call try_make_simple,H,D BB)")
+$(error test failed!)
+endif
+
+ifneq ("12+a b+4","$(value H)")
+$(error test failed!)
+endif
+
+ifneq ("simple","$(flavor H)")
+$(error test failed!)
+endif
 
 #######################################
 # test 'keyed_redefine'
 
+A := 1
 
+ifneq ("","$(call keyed_redefine,A,K,k1,2)")
+$(error test failed!)
+endif
 
+K := kd
+
+ifneq ("1","$A")
+$(error test failed!)
+endif
+
+K := k1
+
+ifneq ("2","$A")
+$(error test failed!)
+endif
 
 #######################################
 all:
