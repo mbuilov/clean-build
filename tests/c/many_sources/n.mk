c5a8c88e9d8bc9044b8aec52ef9d648fd95bf23a
Michael Builov <mbuilov@gmail.com>
2018-02-27 11:51:55 +0300

    fix comment

--- tests/c/many_sources/n.mk
+++ tests/c/many_sources/n.mk
@@ -6,7 +6,10 @@
 
 # make list of generated sources
 
-# included by: gen.mk, lib.mk
+# included by: gen.mk, lib.mk, exe.mk, dll.mk
+
+# where test sources are generated
+test_gen_dir := $(gen_dir)/tests/c/many_sources
 
 # will generate 1000 sources
 # sequence: 1 1 1 1 ...
@@ -17,9 +20,6 @@ n := $n $n $n $n $n $n $n $n $n $n
 
 # sequence: 0 1 2 3 ...
 seq := $(eval c:=)$(foreach i,$n,$(words $c)$(eval c+=1))
-
-# where test sources are generated
-test_gen_dir := $(gen_dir)/tests/c/many_sources
 
 # make list of generated sources: $(gen_dir)/tests/c/many_sources/f0.c $(gen_dir)/tests/c/many_sources/f1.c ...
 generated := $(patsubst %,$(test_gen_dir)/f%.c,$(seq))
