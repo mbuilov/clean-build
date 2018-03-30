528510b065e29e8065e84c1da5ede603414f543f
Michael Builov <mbuilov@gmail.com>
2018-03-30 13:56:48 +0300

    write_text -> sh_write_text, change_mode -> sh_chmod_some_files

--- install/impl/inst_text.mk
+++ install/impl/inst_text.mk
@@ -6,15 +6,16 @@
 
 # write lines of text $1 to the file $2 by $3 lines at one time, then set access mode of written file $2 to $4
 # note: pass non-empty 3-d argument to 'suppress' function to not colorize tool arguments
-# note: 'write_text' - defined in $(utils_mk) makefile (such as $(cb_dir)/utils/unix.mk)
-install_text = $(call suppress,GEN,$2,1)$(write_text)
+# note: 'sh_write_text' - defined in $(utils_mk) makefile (such as $(cb_dir)/utils/unix.mk)
+install_text = $(call suppress,GEN,$2,1)$(sh_write_text)
 
 # set access mode of the file $2 to $4
-# note: 'change_mode' defined in $(cb_dir)/utils/unix.mk, if it was included by $(cb_dir)/core/_defs.mk
-ifdef change_mode
-$(call define_append,install_text,$(newline)$$(call suppress,CHMOD,$$2,1)$$(call change_mode,$$4,$$2))
+# note: pass non-empty 3-d argument to 'suppress' function to not colorize tool arguments
+# note: 'sh_chmod_some_files' defined in $(cb_dir)/utils/unix.mk, if it was included by $(cb_dir)/core/_defs.mk
+ifdef sh_chmod_some_files
+$(call define_append,install_text,$(newline)$$(call suppress_more,CHMOD,$$2,1)$$(call sh_chmod_some_files,$$4,$$2))
 endif
 
 # protect macros from modifications in target makefiles, allow tracing calls to them
-# note: trace namespace: inst_text
-$(call set_global,install_text,inst_text)
+# note: trace namespace: install_text
+$(call set_global,install_text,install_text)
