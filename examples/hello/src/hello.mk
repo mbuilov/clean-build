815d13d70e1ebc74f653ddf86137c1e8c1406c5a
Michael Builov <mbuilov@gmail.com>
2018-04-03 18:26:35 +0300

    preliminary changes

--- examples/hello/src/hello.mk
+++ examples/hello/src/hello.mk
@@ -4,13 +4,16 @@
 # Licensed under GPL version 3 or any later version, see COPYING
 #----------------------------------------------------------------------------------
 
-# add rules for building C/C++ sources
+# add rules (target templates) for building C/C++ sources
 include $(dir $(lastword $(MAKEFILE_LIST)))../make/c.mk
 
-# include definition of 'exe_test_rule_r' macro - which generates a rule used for testing built executables
+# include definition of 'exe_test_rule_r' macro - which generates a rule used for testing built C/C++ executables
 include $(CBLD_ROOT)/extensions/ctest.mk
 
 # we will build S-variant of 'hello' executable - the one which is statically linked with the C runtime
+# note: variable 'exe' - specifies the name of built target executable and its optional variants
+# note: variable 'src' - specifies a list of C/C++ sources from which to build the target (executable)
+# note: variables 'exe' and 'src' - processed by the target templates added above via "include ...make/c.mk"
 exe := hello S
 src := hello.c
 
@@ -18,26 +21,27 @@ src := hello.c
 ifneq (,$(filter check,$(MAKECMDGOALS)))
 
 # define rules for testing built executable and creating 'hello.out' output file
-# note: 'exe_test_rule_r' returns an absolute path to the generated output file - save returned path in "local" variable 'out'
-# note: the same path value may be obtained as: $(addsuffix .out,$(call all_targets,exe))
-# note: 'exe_test_rule_r' macro uses a value of defined above 'exe' variable
-out := $(exe_test_rule_r)
+?# note: 'exe_test_rule_r' returns an absolute path to the generated output file - save returned path in "local" variable 'out'
+?# note: the same path value may be obtained as: $(addsuffix .out,$(call all_targets,exe))
+?# note: 'exe_test_rule_r' macro uses a value of defined above 'exe' variable
+?out := $(exe_test_rule_r)
 
 # define custom rule - print output of tested executable to stderr, for this:
-# 1) set makefile information for 'hello' (an introduced phony target) - this information is used by the 'suppress' function
-#   in "makefile info" mode (enabled via "$(MAKE) M=1"); 'suppress' function pretty-prints what a rule is doing
-# 2) register 'hello' as a leaf target - in those rule the 'suppress' function is used for updating percent of building targets
+# 1) call 'set_makefile_info_r' - set makefile information for 'hello' (an introduced phony target) - this information is used by the
+#  'suppress' function in "makefile info" mode (enabled via "$(MAKE) M=1"); 'suppress' function pretty-prints what a rule is doing
+# 2) call 'suppress_targets_r' - register 'hello' as a leaf target - in those rule the 'suppress' function is used for updating percent
+#  of building targets
 #
-# Note: output of rules must go to stderr, stdout is used by clean-build only for logging executed commands - this is needed
-#  for the (optional) generation of a build-script
+# Note: output of any rule must go to stderr (i.e. >&2) or to file (e.g. >file), stdout is used exclusively by clean-build
+#  only for logging executed commands - this is needed for the (optional) generation of a build-script (in "verbose" mode)
 # Note: must not use "local" variables (e.g. 'out') in rule bodies - may use only target-specific, automatic or "global"
 #  (registered by 'set_global' macro) variables, here use $| (automatic variable) - list of order-only dependencies of the target,
 #  for this rule it contains only the $(out)
 # Note: 'sh_cat' - one of clean-build defined shell utilities functions
-$(call suppress_targets_r,$(call set_makefile_info_r,hello)): | $(out)
+$(call suppress_targets_r,$(call set_makefile_info_r,hello)):| $(out)
 	$(call suppress,CAT,$|)$(call sh_cat,$|) >&2
 
-# to complete clean-build predefined 'check' goal, it is needed to update our target 'hello'
+# to complete clean-build predefined 'check' goal, it is needed to update our phony target 'hello'
 check: hello
 
 # specify that 'hello' - is not a file, it is a PHONY target
@@ -45,5 +49,5 @@ check: hello
 
 endif # check
 
-# define targets and rules how to build them
+# expand target templates - define target files and rules how to build them
 $(define_targets)
